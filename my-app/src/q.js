e350592bb3ad1d458b530706427b52441f80429f
evnwttn <evnwttn@gmail.com>
2022-02-19 17:40:47 -0330

    Update q.js

--- my-app/src/q.js
+++ my-app/src/q.js
@@ -3,7 +3,7 @@
 //  best way to approach meta tags in react,
 //  ie. <meta name="viewport" content="initial-scale=1, width=device-width" />
 
-//
+// if styling correctly/combining modal/buttons was a good shout
 //
 
 //
