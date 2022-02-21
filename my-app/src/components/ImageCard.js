55e244b0924203cbe2ec4000e20880ea5421125d
evnwttn <evnwttn@gmail.com>
2022-02-21 15:38:24 -0330

    Update ImageCard.js

--- my-app/src/components/ImageCard.js
+++ my-app/src/components/ImageCard.js
@@ -1,7 +1,37 @@
 import React from "react";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@mui/material/";
 
 const ImageCard = () => {
-  return <div>Hello from ImageCard</div>;
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default ImageCard;
