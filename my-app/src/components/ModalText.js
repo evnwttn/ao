f0fa9161972d75a433bb6abf9d4e40951c72905c
evnwttn <evnwttn@gmail.com>
2022-02-23 18:50:00 -0330

    Update ModalText.js

--- my-app/src/components/ModalText.js
+++ my-app/src/components/ModalText.js
@@ -1,1 +1,44 @@
 import React from "react";
+import { Divider, Box } from "@mui/material";
+import "@fontsource/noto-sans";
+import "@fontsource/noto-serif";
+import { aoWhite, aoRed, aoGrey, aoDivider } from "../assets/theme";
+
+const style = {
+  textTransform: "uppercase",
+  fontSize: "0.8rem",
+  letterSpacing: "0.2pt",
+  textAlign: "center",
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "33%",
+  bgcolor: aoGrey,
+  boxShadow: "0px 0px 200px",
+  p: 4,
+};
+
+export const ModalText = ({ text }) => {
+  return (
+    <Box sx={style}>
+      <div
+        style={{
+          fontSize: "2.5rem",
+          fontFamily: "Noto Serif",
+          textTransform: "lowercase",
+          color: aoRed,
+        }}
+      >
+        ꜵ
+      </div>
+
+      <Divider variant="middle" style={{ color: aoDivider }} />
+
+      <br />
+      <div style={{ fontFamily: "Noto Sans", color: aoWhite }}>
+        This be the {text} modal and it'll show {text} shit.
+      </div>
+    </Box>
+  );
+};
