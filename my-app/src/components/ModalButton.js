e63314d2349da1020fb002be1ee3443dc5fd5b81
evnwttn <evnwttn@gmail.com>
2022-02-22 21:03:34 -0330

    revert for meow

--- my-app/src/components/ModalButton.js
+++ my-app/src/components/ModalButton.js
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import "@fontsource/noto-sans";
 import "@fontsource/noto-serif";
 import { Divider, Button, Box, Modal } from "@mui/material";
@@ -24,5 +24,40 @@ export const ModalButton = ({ text }) => {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  return <></>;
+  return (
+    <>
+      <Button key={text} onClick={handleOpen} variant="text" color="secondary">
+        {text}
+      </Button>
+
+      {/*  */}
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <div
+            style={{
+              fontSize: "2.5rem",
+              fontFamily: "Noto Serif",
+              textTransform: "lowercase",
+              color: aoRed,
+            }}
+          >
+            ꜵ
+          </div>
+
+          <Divider variant="middle" style={{ color: aoDivider }} />
+
+          <br />
+          <div style={{ fontFamily: "Noto Sans", color: aoWhite }}>
+            This be the {text} modal and it'll show {text} shit.
+          </div>
+        </Box>
+      </Modal>
+    </>
+  );
 };
