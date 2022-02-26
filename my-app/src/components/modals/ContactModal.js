252303b2ae0aea8a1294385d40d2abdf70c4c7ba
evnwttn <evnwttn@gmail.com>
2022-02-26 15:11:11 -0330

    Update ContactModal.js

--- my-app/src/components/modals/ContactModal.js
+++ my-app/src/components/modals/ContactModal.js
@@ -4,8 +4,8 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 export const ContactModal = () => {
   return (
     <>
-      <AlternateEmailIcon color="secondary" />
-      <GitHubIcon color="secondary" />
+      <AlternateEmailIcon sx={{ mx: 1 }} color="secondary" />
+      <GitHubIcon sx={{ mx: 1 }} color="secondary" />
     </>
   );
 };
