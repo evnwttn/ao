0f4ac45cde7678c61a674282986e3ebc43fda8fc
evnwttn <evnwttn@gmail.com>
2022-02-28 14:15:32 -0330

    aboot

--- my-app/src/components/modals/NewModal.js
+++ my-app/src/components/modals/NewModal.js
@@ -1,3 +1,11 @@
+import { Box, TextField } from "@mui/material";
+import AddCircleOutline from "@mui/icons-material/AddCircleOutline";
+
 export const NewModal = () => {
-  return null;
+  return (
+    <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+      <AddCircleOutline sx={{ color: "action.active", mr: 1, my: 0.5 }} />
+      <TextField id="input-with-sx" label="With sx" variant="standard" />
+    </Box>
+  );
 };
