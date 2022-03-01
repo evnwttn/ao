ea560bebafd87047cbbf8456b3b0d5c0bf653d22
evnwttn <evnwttn@gmail.com>
2022-03-01 13:35:40 -0330

    clean dat import why doncha

--- my-app/src/components/modals/LoadModal.js
+++ my-app/src/components/modals/LoadModal.js
@@ -1,11 +1,12 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import Divider from "@mui/material/Divider";
+import React from "react";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 
 export const LoadModal = () => {
