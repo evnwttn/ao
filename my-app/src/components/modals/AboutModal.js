b6336829ecf53ba11c1e4b45144e86e1e92f9c51
evnwttn <evnwttn@gmail.com>
2022-03-01 20:28:29 -0330

    ah yes perfect

--- my-app/src/components/modals/AboutModal.js
+++ my-app/src/components/modals/AboutModal.js
@@ -1,3 +1,15 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
 export const AboutModal = () => {
-  return null;
+  return (
+    <Box>
+      <Typography variant="h1" component="div" gutterBottom>
+        h1. Heading
+      </Typography>
+      <Typography variant="h2" gutterBottom component="div">
+        h2. Heading
+      </Typography>
+    </Box>
+  );
 };
