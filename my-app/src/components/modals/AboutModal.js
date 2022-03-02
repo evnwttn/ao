a439f07c61ae64ebeea3d30c15eba8b426f41402
evnwttn <evnwttn@gmail.com>
2022-03-01 20:34:10 -0330

    Update AboutModal.js

--- my-app/src/components/modals/AboutModal.js
+++ my-app/src/components/modals/AboutModal.js
@@ -2,14 +2,5 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
 export const AboutModal = () => {
-  return (
-    <Box>
-      <Typography variant="h1" component="div" gutterBottom>
-        h1. Heading
-      </Typography>
-      <Typography variant="h2" gutterBottom component="div">
-        h2. Heading
-      </Typography>
-    </Box>
-  );
+  return <Box></Box>;
 };
