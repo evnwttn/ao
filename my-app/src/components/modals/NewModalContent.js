7d916079948cf5a4b94e837c090d739702a4655f
evnwttn <evnwttn@gmail.com>
2022-03-07 16:38:24 -0330

    no one library should have all that powa

--- my-app/src/components/modals/NewModalContent.js
+++ my-app/src/components/modals/NewModalContent.js
@@ -2,8 +2,23 @@ import React, { useState } from "react";
 import { Box, TextField, IconButton } from "@mui/material";
 import { useForm } from "react-hook-form";
 import SendIcon from "@mui/icons-material/Send";
-// import AddCircleOutline from "@mui/icons-material/AddCircleOutline";
+import AddCircleOutline from "@mui/icons-material/AddCircleOutline";
 import { palette } from "../../assets/theme";
+
+const boxStyle = {
+  display: "flex",
+  alignItems: "flex-end",
+  justifyContent: "center",
+};
+
+const fontStyle = {
+  fontWeight: "400",
+  fontSize: "0.75rem",
+  lineHeight: "1.66",
+  fontFamily: "Noto Sans",
+  letterSpacing: "0.03333em",
+  color: palette.aoWhite,
+};
 
 export const NewModalContent = () => {
   const [titleFormCompleted, setTitleFormCompleted] = useState(false);
@@ -27,7 +42,15 @@ export const NewModalContent = () => {
   return (
     <>
       {titleFormCompleted ? (
-        <>{aoSessionTitle}</>
+        <Box sx={{ boxStyle, fontStyle }}>
+          Enter the titles of tracks you wish to include in the {aoSessionTitle}{" "}
+          session. You can always add more later.
+          <br />
+          <AddCircleOutline
+            onClick={() => console.log(aoSessionTitle)}
+            sx={{ color: palette.aoWhite, mr: 1, my: 0.5, cursor: "pointer" }}
+          />
+        </Box>
       ) : (
         <>
           <Box component="form" onSubmit={handleSubmit(onSubmit)}>
@@ -55,29 +78,3 @@ export const NewModalContent = () => {
     </>
   );
 };
-
-// {titleFormCompleted ? (
-//   <Box sx={{ boxStyle, fontStyle }}>
-//     Enter the titles of tracks you wish to include in the{" "}
-//     {sessionTitle + " "} session. You can always add more later.
-//     <br />
-//     <AddCircleOutline
-//       onClick={() => console.log(sessionTitle)}
-//       sx={{ color: palette.aoWhite, mr: 1, my: 0.5, cursor: "pointer" }}
-//     />
-//   </Box>
-
-// // const boxStyle = {
-// //   display: "flex",
-// //   alignItems: "flex-end",
-// //   justifyContent: "center",
-// // };
-
-// // const fontStyle = {
-// //   fontWeight: "400",
-// //   fontSize: "0.75rem",
-// //   lineHeight: "1.66",
-// //   fontFamily: "Noto Sans",
-// //   letterSpacing: "0.03333em",
-// //   color: palette.aoWhite,
-// // };
