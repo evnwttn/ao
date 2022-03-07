1756e100efc1a5d0cdfc8881bf83f482dfdbe02e
evnwttn <evnwttn@gmail.com>
2022-03-07 12:23:41 -0330

    post pre but pre post

--- my-app/src/components/modals/ContactModalContent.js
+++ my-app/src/components/modals/ContactModalContent.js
@@ -1,10 +1,30 @@
-import SendIcon from "@mui/icons-material/Send";
-import { palette } from "../../assets/theme";
+import React from "react";
+import { useForm } from "react-hook-form";
+// import { palette } from "../../assets/theme";
+// import SendIcon from "@mui/icons-material/Send";
 
 export const ContactModalContent = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
   return (
-    <>
-      <SendIcon sx={{ color: palette.aoWhite, my: 0.5 }} />
-    </>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="Name" {...register("Name", {})} />
+      <input
+        type="email"
+        placeholder="Email Address"
+        {...register("Email Address", {})}
+      />
+      <input type="text" placeholder="Message" {...register("Message", {})} />
+
+      <input type="submit" />
+    </form>
   );
 };
+
+// <SendIcon sx={{ color: palette.aoWhite, my: 0.5 }} />;
