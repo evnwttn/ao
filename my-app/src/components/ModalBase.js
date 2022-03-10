7cbd13f0fe82aa08861e0852952a369ffa3c2379
evnwttn <evnwttn@gmail.com>
2022-03-10 19:23:02 -0330

    da base

---
+++ my-app/src/components/ModalBase.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Modal } from "@mui/material";
+import { ModalText } from "../ModalText";
+
+export const ModalBase = ({ open, handleClose, text }) => {
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <ModalText prompt={text} />
+    </Modal>
+  );
+};
