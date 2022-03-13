f1f3c3b61073906f5552285f75ba51732fb61059
evnwttn <evnwttn@gmail.com>
2022-03-13 16:55:31 -0230

    old text pre update

--- my-app/src/components/modals/AboutModalContent.js
+++ my-app/src/components/modals/AboutModalContent.js
@@ -1,5 +1,11 @@
 import Box from "@mui/material/Box";
 
 export const AboutModalContent = () => {
-  return <Box></Box>;
+  return (
+    <Box>
+      A VISUAL ALBUM MANAGEMENT TOOLKIT DESIGNED BY/FOR MUSICIANS TO
+      TRACK/MONITOR PROGRESS, FOSTER COLLABORATION AND AID IN THE SMASHING OF
+      GOALS. TO BEGIN START A NEW PROJECT
+    </Box>
+  );
 };
