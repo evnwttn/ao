1caa75bf16d5970ab66962e599d22700896421c5
evnwttn <evnwttn@gmail.com>
2022-03-14 11:08:58 -0230

    Update AboutModalContent.js

--- my-app/src/components/modals/AboutModalContent.js
+++ my-app/src/components/modals/AboutModalContent.js
@@ -1,11 +1,23 @@
 import Box from "@mui/material/Box";
+import { palette } from "../../assets/theme";
 
 export const AboutModalContent = () => {
   return (
-    <Box>
-      A VISUAL ALBUM MANAGEMENT TOOLKIT DESIGNED BY/FOR MUSICIANS TO
-      TRACK/MONITOR PROGRESS, FOSTER COLLABORATION AND AID IN THE SMASHING OF
-      GOALS. TO BEGIN START A NEW PROJECT
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        fontWeight: "400",
+        fontSize: "0.75rem",
+        lineHeight: "1.66",
+        fontFamily: "Noto Sans",
+        letterSpacing: "0.03333em",
+        color: palette.aoWhite,
+      }}
+    >
+      A VISUAL ALBUM MANAGEMENT TOOLKIT DESIGNED BY &amp; FOR MUSICIANS TO TRACK
+      &amp; MONITOR PROGRESS, FOSTER COLLABORATION AND AID IN THE SMASHING OF
+      GOALS. TO BEGIN START A NEW PROJECT.
     </Box>
   );
 };
