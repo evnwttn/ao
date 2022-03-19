07d8bf3f10b795434a52307efb3552937de418f2
evnwttn <evnwttn@gmail.com>
2022-03-19 16:02:34 -0230

    sometimes after 4 hours you gotta gut it

--- my-app/src/components/modals/NewModalContentList.js
+++ my-app/src/components/modals/NewModalContentList.js
@@ -1,39 +1,17 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
 import { Box, IconButton, List, ListItem, ListItemText } from "@mui/material";
 import ClearIcon from "@mui/icons-material/Clear";
 import { palette } from "../../assets/theme";
 
 export const NewModalContentList = ({ aoOutput, target }) => {
-  const [aoUpdate, setAoUpdate] = useState([]);
-  const [formTarget, setFormTarget] = useState(`track`);
-  const [formNumber, setFormNumber] = useState(0);
-
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
-
-  const onSubmit = (data) => {
-    console.log(data);
-    setFormNumber(formNumber + 1);
-  };
-
   return (
-    <Box component="form" onSubmit={handleSubmit(onSubmit)}>
+    <Box>
       {aoOutput.track !== undefined ? (
         <List sx={{ width: "100%", bgcolor: palette.aoBlack }}>
           {aoOutput.track.map((title) => (
             <ListItem
               key={title}
               secondaryAction={
-                <IconButton
-                  value={title}
-                  {...register(`${formTarget}.${formNumber}`)}
-                  type="submit"
-                >
+                <IconButton onClick={() => console.log(title)}>
                   <ClearIcon />
                 </IconButton>
               }
@@ -55,5 +33,3 @@ export const NewModalContentList = ({ aoOutput, target }) => {
 //     aoOutput.track.splice(toRemove, 1);
 //     setAoUpdate(aoOutput);
 //     console.log({ aoUpdate });
-
-// setAoOutput({ ...data });
