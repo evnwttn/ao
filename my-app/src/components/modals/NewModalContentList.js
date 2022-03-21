5ab6bf663d96a6f4646eb6d6729b487e8391dec9
evnwttn <evnwttn@gmail.com>
2022-03-21 11:54:40 -0230

    Update NewModalContentList.js

--- my-app/src/components/modals/NewModalContentList.js
+++ my-app/src/components/modals/NewModalContentList.js
@@ -12,7 +12,7 @@ export const NewModalContentList = ({ input, target, onClick }) => {
               <ListItem
                 key={title}
                 secondaryAction={
-                  <IconButton onClick={() => console.log(title)}>
+                  <IconButton onClick={onClick}>
                     <ClearIcon />
                   </IconButton>
                 }
@@ -29,7 +29,7 @@ export const NewModalContentList = ({ input, target, onClick }) => {
               <ListItem
                 key={title}
                 secondaryAction={
-                  <IconButton onClick={() => console.log(title)}>
+                  <IconButton onClick={onClick}>
                     <ClearIcon />
                   </IconButton>
                 }
