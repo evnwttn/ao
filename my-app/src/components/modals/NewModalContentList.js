6d64348d7175993b75633cab53dd84b80b18c0c0
evnwttn <evnwttn@gmail.com>
2022-03-27 13:18:56 -0230

    pre jimmy jangle

--- my-app/src/components/modals/NewModalContentList.js
+++ my-app/src/components/modals/NewModalContentList.js
@@ -2,27 +2,26 @@ import { Box, IconButton, List, ListItem, ListItemText } from "@mui/material";
 import ClearIcon from "@mui/icons-material/Clear";
 import { palette } from "../../assets/theme";
 
-// will complete when gf is done being angry about taxes
-// const renderList = ({ input, target, onSubmit }) => {
-//   return (
-//     <List sx={{ width: "100%", bgcolor: palette.aoBlack }}>
-//       {input.track.map((title) => (
-//         <ListItem
-//           key={title}
-//           secondaryAction={
-//             <IconButton onClick={() => onSubmit(title)}>
-//               <ClearIcon />
-//             </IconButton>
-//           }
-//         >
-//           <ListItemText primary={`${title}`} />
-//         </ListItem>
-//       ))}
-//     </List>
-//   );
-// };
-
 export const NewModalContentList = ({ input, target, onSubmit }) => {
+  const renderList = (elm) => {
+    return (
+      <List sx={{ width: "100%", bgcolor: palette.aoBlack }}>
+        {input.elm.map((title) => (
+          <ListItem
+            key={title}
+            secondaryAction={
+              <IconButton onClick={() => onSubmit(title)}>
+                <ClearIcon />
+              </IconButton>
+            }
+          >
+            <ListItemText primary={`${title}`} />
+          </ListItem>
+        ))}
+      </List>
+    );
+  };
+
   return (
     <Box>
       {target === `track` ? (
