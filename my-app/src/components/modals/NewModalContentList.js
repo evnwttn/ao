55f3218dd81ecb1f6c05b412bb3d25dafc449a00
evnwttn <evnwttn@gmail.com>
2022-03-27 13:32:12 -0230

    doesnt work

--- my-app/src/components/modals/NewModalContentList.js
+++ my-app/src/components/modals/NewModalContentList.js
@@ -3,25 +3,6 @@ import ClearIcon from "@mui/icons-material/Clear";
 import { palette } from "../../assets/theme";
 
 export const NewModalContentList = ({ input, target, onSubmit }) => {
-  const renderList = (elm) => {
-    return (
-      <List sx={{ width: "100%", bgcolor: palette.aoBlack }}>
-        {input.elm.map((title) => (
-          <ListItem
-            key={title}
-            secondaryAction={
-              <IconButton onClick={() => onSubmit(title)}>
-                <ClearIcon />
-              </IconButton>
-            }
-          >
-            <ListItemText primary={`${title}`} />
-          </ListItem>
-        ))}
-      </List>
-    );
-  };
-
   return (
     <Box>
       {target === `track` ? (
