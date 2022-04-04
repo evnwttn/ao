efbc8fbaed018754d98e9bfb68261f97711659ae
evnwttn <evnwttn@gmail.com>
2022-04-04 15:37:19 -0230

    sample session

--- my-app/src/components/AOGrid.js
+++ my-app/src/components/AOGrid.js
@@ -1,5 +1,33 @@
 import React from "react";
 
+const arr = {
+  sessionTitle: "the dark side of the moon",
+  track: [
+    "speak to me",
+    "breathe (in the air)",
+    "on the run",
+    "time",
+    "the great gig in the sky",
+    "money",
+    "us and them",
+    "any colour you like",
+    "brain damage",
+    "eclipse",
+  ],
+  parameter: [
+    "vocals",
+    "guitar",
+    "bass",
+    "drums",
+    "piano",
+    "guest players",
+    "guest vocals",
+    "fx",
+    "mix",
+    "master",
+  ],
+};
+
 export const AOGrid = () => {
-  return <>{console.log("Hello World")}</>;
+  return <>{console.log(arr)}</>;
 };
