dcd19b3ee81328d3ffcc1b21538b96ada17ee31b
evnwttn <evnwttn@gmail.com>
2022-04-04 13:20:14 -0230

    Update NewModalContentList.js

--- my-app/src/components/modals/NewModalContentList.js
+++ my-app/src/components/modals/NewModalContentList.js
@@ -7,7 +7,7 @@ export const NewModalContentList = ({ input, target, onSubmit }) => {
   return (
     <Box sx={{ ...modalCenteredSx, my: 1 }}>
       <List sx={{ width: "85%", bgcolor: palette.aoBlack }}>
-        {target === `track`
+        {target !== `parameter`
           ? input.track !== undefined
             ? input.track.map((value) => {
                 return (
@@ -24,23 +24,21 @@ export const NewModalContentList = ({ input, target, onSubmit }) => {
                 );
               })
             : null
-          : target === `parameter`
-          ? input.parameter !== undefined
-            ? input.parameter.map((value) => {
-                return (
-                  <ListItem
-                    key={value}
-                    secondaryAction={
-                      <IconButton onClick={() => console.log(value)}>
-                        <ClearIcon />
-                      </IconButton>
-                    }
-                  >
-                    <ListItemText primary={value} />
-                  </ListItem>
-                );
-              })
-            : null
+          : input.parameter !== undefined
+          ? input.parameter.map((value) => {
+              return (
+                <ListItem
+                  key={value}
+                  secondaryAction={
+                    <IconButton onClick={() => console.log(value)}>
+                      <ClearIcon />
+                    </IconButton>
+                  }
+                >
+                  <ListItemText primary={value} />
+                </ListItem>
+              );
+            })
           : null}
       </List>
     </Box>
