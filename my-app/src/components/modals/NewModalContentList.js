71278a64904dd31313ff7c5adbb6d88c01269639
evnwttn <evnwttn@gmail.com>
2022-04-04 12:33:50 -0230

    rejigging to find

--- my-app/src/components/modals/NewModalContentList.js
+++ my-app/src/components/modals/NewModalContentList.js
@@ -6,46 +6,77 @@ import { modalCenteredSx } from "../../assets/theme";
 export const NewModalContentList = ({ input, target, onSubmit }) => {
   return (
     <Box sx={{ ...modalCenteredSx, my: 1 }}>
-      {target === `track` ? (
-        input.track !== undefined ? (
-          <List sx={{ width: "85%", bgcolor: palette.aoBlack }}>
-            {input.track.map((title) => (
-              <ListItem
-                key={title}
-                secondaryAction={
-                  <IconButton onClick={() => onSubmit(title)}>
-                    <ClearIcon />
-                  </IconButton>
-                }
-              >
-                <ListItemText primary={`${title}`} />
-              </ListItem>
-            ))}
-          </List>
-        ) : null
-      ) : target === `parameter` ? (
-        input.parameter === `` || undefined ? null : (
-          console.log(input.parameter)
-        )
-      ) : // (
-      //   input.parameter !== `` && undefined ? (
-      // <List sx={{ width: "85%", bgcolor: palette.aoBlack }}>
-      //   {input.parameter.map((title) => (
-      //     <ListItem
-      //       key={title}
-      //       secondaryAction={
-      //         <IconButton onClick={() => onSubmit(title)}>
-      //           <ClearIcon />
-      //         </IconButton>
-      //       }
-      //     >
-      //       <ListItemText primary={`${title}`} />
-      //     </ListItem>
-      //   ))}
-      // </List>
-      //   ) : null
-      // )
-      null}
+      <List sx={{ width: "85%", bgcolor: palette.aoBlack }}>
+        <ListItem
+          secondaryAction={
+            <IconButton onClick={() => console.log("yo")}>
+              <ClearIcon />
+            </IconButton>
+          }
+        >
+          <ListItemText primary={`Hello`} />
+        </ListItem>
+      </List>
     </Box>
   );
 };
+
+// export const NewModalContentList = ({ input, target, onSubmit }) => {
+//   return (
+//     <Box sx={{ ...modalCenteredSx, my: 1 }}>
+//       {target === `track` ? (
+//         input.track !== undefined ? (
+//           <List sx={{ width: "85%", bgcolor: palette.aoBlack }}>
+//             {input.track.map((title) => (
+//               <ListItem
+//                 key={title}
+//                 secondaryAction={
+//                   <IconButton onClick={() => onSubmit(title)}>
+//                     <ClearIcon />
+//                   </IconButton>
+//                 }
+//               >
+//                 <ListItemText primary={`${title}`} />
+//               </ListItem>
+//             ))}
+//           </List>
+//         ) : null
+//       ) : target === `parameter` ? (
+//         input.parameter === `` || undefined ? null : (
+//           <List sx={{ width: "85%", bgcolor: palette.aoBlack }}>
+//             {input.parameter.map((title) => (
+//               <ListItem
+//                 key={title}
+//                 secondaryAction={
+//                   <IconButton onClick={() => onSubmit(title)}>
+//                     <ClearIcon />
+//                   </IconButton>
+//                 }
+//               >
+//                 <ListItemText primary={`${title}`} />
+//               </ListItem>
+//             ))}
+//           </List>
+//         )
+//       ) : // (
+//       //   input.parameter !== `` && undefined ? (
+//       // <List sx={{ width: "85%", bgcolor: palette.aoBlack }}>
+//       //   {input.parameter.map((title) => (
+//       //     <ListItem
+//       //       key={title}
+//       //       secondaryAction={
+//       //         <IconButton onClick={() => onSubmit(title)}>
+//       //           <ClearIcon />
+//       //         </IconButton>
+//       //       }
+//       //     >
+//       //       <ListItemText primary={`${title}`} />
+//       //     </ListItem>
+//       //   ))}
+//       // </List>
+//       //   ) : null
+//       // )
+//       null}
+//     </Box>
+//   );
+// };
