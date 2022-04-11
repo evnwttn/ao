b03c336ccac7d7d0557b05c70d9ad9bb4c132832
evnwttn <evnwttn@gmail.com>
2022-04-11 16:04:55 -0230

    WOOOOOO

--- my-app/src/components/AOGrid.js
+++ my-app/src/components/AOGrid.js
@@ -5,19 +5,18 @@ import { palette, gridDomSx, cellSx } from "../assets/theme";
 import { data } from "../assets/TestData";
 
 export const AOGrid = () => {
-  const [cellColour, toggleCellColour] = useState(palette.aoBlue);
-
-  const handleCellClick = (input) => {
-    {
-      cellColour === palette.aoBlue && toggleCellColour("transparent");
-      cellColour === "transparent" && toggleCellColour(palette.aoGrey);
-      cellColour === palette.aoGrey && toggleCellColour(palette.aoYellow);
-      cellColour === palette.aoYellow && toggleCellColour(palette.aoBlue);
-    }
-    console.log(input);
-  };
-
   const AOCell = (cell) => {
+    const [cellColour, toggleCellColour] = useState("transparent");
+
+    const handleCellClick = (input) => {
+      {
+        cellColour === palette.aoBlue && toggleCellColour("transparent");
+        cellColour === "transparent" && toggleCellColour(palette.aoGrey);
+        cellColour === palette.aoGrey && toggleCellColour(palette.aoYellow);
+        cellColour === palette.aoYellow && toggleCellColour(palette.aoBlue);
+      }
+      console.log(input);
+    };
     return (
       <Grid item sm={1} onClick={() => handleCellClick(cell)}>
         <Box sx={{ ...cellSx.cell, backgroundColor: cellColour }}></Box>
