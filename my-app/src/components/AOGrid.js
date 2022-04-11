eaf4f49aff294c25e342c40c15ad9a3be27b3152
evnwttn <evnwttn@gmail.com>
2022-04-11 11:54:24 -0230

    theme import

--- my-app/src/components/AOGrid.js
+++ my-app/src/components/AOGrid.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Box, Grid } from "@mui/material/";
+import { Box, Grid, ThemeProvider } from "@mui/material/";
+import aotheme from "../assets/theme";
 import { palette } from "../assets/theme";
 import { data } from "../assets/TestData";
 
@@ -18,46 +19,48 @@ const logCell = (input) => {
 
 export const AOGrid = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      {data.sessionTitle}
-      <Grid container>
-        <Grid container spacing={2} direction="row">
-          {data.parameters.map((parameter) => {
+    <ThemeProvider theme={aotheme}>
+      <Box sx={{ flexGrow: 1 }}>
+        {data.sessionTitle}
+        <Grid container>
+          <Grid container spacing={2} direction="row">
+            {data.parameters.map((parameter) => {
+              return (
+                <Grid item sm={1} key={parameter}>
+                  <Box sx={styling.cell} onClick={() => logCell(parameter)}>
+                    {parameter}
+                  </Box>
+                </Grid>
+              );
+            })}
+          </Grid>
+          {data.tracks.map((track) => {
             return (
-              <Grid item sm={1} key={parameter}>
-                <Box sx={styling.cell} onClick={() => logCell(parameter)}>
-                  {parameter}
-                </Box>
+              <Grid container key={track} spacing={2} direction="row">
+                {data.parameters.map((cell) => {
+                  return cell === `tracks` ? (
+                    <Grid item sm={1} sx={{ my: 2 }} key={cell}>
+                      <Box
+                        sx={styling.cell}
+                        onClick={() => logCell(`${track} + ${cell}`)}
+                      >
+                        {track}
+                      </Box>
+                    </Grid>
+                  ) : (
+                    <Grid item sm={1} sx={{ my: 2 }} key={cell}>
+                      <Box
+                        sx={styling.cell}
+                        onClick={() => logCell(`${track} + ${cell}`)}
+                      ></Box>
+                    </Grid>
+                  );
+                })}
               </Grid>
             );
           })}
         </Grid>
-        {data.tracks.map((track) => {
-          return (
-            <Grid container key={track} spacing={2} direction="row">
-              {data.parameters.map((cell) => {
-                return cell === `tracks` ? (
-                  <Grid item sm={1} sx={{ my: 2 }} key={cell}>
-                    <Box
-                      sx={styling.cell}
-                      onClick={() => logCell(`${track} + ${cell}`)}
-                    >
-                      {track}
-                    </Box>
-                  </Grid>
-                ) : (
-                  <Grid item sm={1} sx={{ my: 2 }} key={cell}>
-                    <Box
-                      sx={styling.cell}
-                      onClick={() => logCell(`${track} + ${cell}`)}
-                    ></Box>
-                  </Grid>
-                );
-              })}
-            </Grid>
-          );
-        })}
-      </Grid>
-    </Box>
+      </Box>
+    </ThemeProvider>
   );
 };
