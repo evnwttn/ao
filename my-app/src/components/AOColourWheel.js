5d1dad49d4be857d182676cd671d6b3e9717f2b1
evnwttn <evnwttn@gmail.com>
2022-04-19 11:40:25 -0400

    proto wheel base for hookup

--- my-app/src/components/AOColourWheel.js
+++ my-app/src/components/AOColourWheel.js
@@ -1,1 +1,13 @@
-export const AOColourWheel = () => {};
+import { Box, Fab } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import { palette } from "../assets/theme";
+
+export const AOColourWheel = () => {
+  return (
+    <Box>
+      <Fab color={palette.aoBlue} aria-label="add">
+        <AddIcon />
+      </Fab>
+    </Box>
+  );
+};
