59f237440441e8a50dca845368bd57b03586e0a2
evnwttn <evnwttn@gmail.com>
2022-04-19 12:18:58 -0400

    by god

--- my-app/src/components/AOCell.js
+++ my-app/src/components/AOCell.js
@@ -1,41 +1,72 @@
+import React from "react";
 import { useState } from "react";
-import { Box, Grid } from "@mui/material/";
+import { Box, Grid, Popover } from "@mui/material/";
 import { palette, cellSx } from "../assets/theme";
 
 export const AOCell = (cell) => {
-  const [cellColour, toggleCellColour] = useState("transparent");
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const cellColour = palette.aoGrey;
 
-  const handleCellClick = (input) => {
-    switch (cellColour) {
-      case palette.aoBlue:
-        toggleCellColour("transparent");
-        break;
-      case "transparent":
-        toggleCellColour(palette.aoGrey);
-        break;
-      case palette.aoGrey:
-        toggleCellColour(palette.aoYellow);
-        break;
-      case palette.aoYellow:
-        toggleCellColour(palette.aoBlue);
-        break;
-      default:
-    }
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+    console.log(event);
   };
 
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
   return (
-    <Grid item sm={1} onClick={() => handleCellClick(cell)}>
-      <Box
-        sx={{
-          ...cellSx.cell,
-          backgroundColor: cellColour,
-          "&:hover": {
-            backgroundColor: cellColour === "transparent" && palette.aoGrey,
-            opacity: "0.6",
-            cursor: "pointer",
-          },
+    <>
+      <Grid item sm={1} onClick={handleClick}>
+        <Box
+          aria-describedby={cell}
+          sx={{
+            ...cellSx.cell,
+            backgroundColor: cellColour,
+            "&:hover": {
+              backgroundColor: cellColour === "transparent" && palette.aoGrey,
+              opacity: "0.6",
+              cursor: "pointer",
+            },
+          }}
+        ></Box>
+      </Grid>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
         }}
-      ></Box>
-    </Grid>
+      >
+        <>HELLOHELLOHELLO</>
+      </Popover>
+    </>
   );
 };
+
+// const [cellColour, toggleCellColour] = useState("transparent");
+
+// const handleCellClick = (input) => {
+//   switch (cellColour) {
+//     case palette.aoBlue:
+//       toggleCellColour("transparent");
+//       break;
+//     case "transparent":
+//       toggleCellColour(palette.aoGrey);
+//       break;
+//     case palette.aoGrey:
+//       toggleCellColour(palette.aoYellow);
+//       break;
+//     case palette.aoYellow:
+//       toggleCellColour(palette.aoBlue);
+//       break;
+//     default:
+//   }
+// };
--- my-app/src/components/AOColourWheel.js
+++ my-app/src/components/AOColourWheel.js
@@ -1,13 +1,1 @@
-import { Box, Fab } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import { palette } from "../assets/theme";
-
-export const AOColourWheel = () => {
-  return (
-    <Box>
-      <Fab color={palette.aoBlue} aria-label="add">
-        <AddIcon />
-      </Fab>
-    </Box>
-  );
-};
+export const AOColourWheel = () => {};
