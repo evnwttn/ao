6ead43894795f2fb943c185a43ba34caaa7b6cc5
evnwttn <evnwttn@gmail.com>
2022-04-19 16:03:59 -0400

    wheelin dealin

--- my-app/src/components/AOColourWheel.js
+++ my-app/src/components/AOColourWheel.js
@@ -1,18 +1,37 @@
 import SvgIcon from "@mui/material/SvgIcon";
+import { palette } from "../assets/theme";
 
 const Wheel = (props) => {
   return (
-    <SvgIcon sx={{ width: "10vw", height: "10vw" }} {...props}>
-      <svg
-        width="8vw"
-        height="8vw"
-        viewBox="0 0 100 100"
-        xmlns="http://www.w3.org/2000/svg"
-        preserveAspectRatio="xMinYMin"
+    <>
+      <SvgIcon
+        sx={{ width: "10vw", height: "10vw", transform: "rotate(270deg)" }}
+        {...props}
       >
-        <path d="M21.95,11L19.938,11C19.482,7.386 16.614,4.518 13,4.062L13,2.049C17.714,2.524 21.476,6.286 21.95,11Z" />
-      </svg>
-    </SvgIcon>
+        <svg
+          width="8vw"
+          height="8vw"
+          color={palette.aoYellow}
+          viewBox="0vw 0vw 100vw 100vw"
+          xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="xMinYMin"
+        >
+          <path d="M21.95,11L19.938,11C19.482,7.386 16.614,4.518 13,4.062L13,2.049C17.714,2.524 21.476,6.286 21.95,11Z" />
+        </svg>
+      </SvgIcon>
+      <SvgIcon sx={{ width: "10vw", height: "10vw" }} {...props}>
+        <svg
+          width="8vw"
+          height="8vw"
+          color={palette.aoBlue}
+          viewBox="0vw 0vw 100vw 100vw"
+          xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="xMinYMin"
+        >
+          <path d="M21.95,11L19.938,11C19.482,7.386 16.614,4.518 13,4.062L13,2.049C17.714,2.524 21.476,6.286 21.95,11Z" />
+        </svg>
+      </SvgIcon>
+    </>
   );
 };
 
