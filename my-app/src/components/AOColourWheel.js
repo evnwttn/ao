7e30738b66d919327ffa037aadcde30303996bd4
evnwttn <evnwttn@gmail.com>
2022-04-19 12:58:06 -0400

    SVG

--- my-app/src/components/AOColourWheel.js
+++ my-app/src/components/AOColourWheel.js
@@ -1,4 +1,13 @@
 import { Box } from "@mui/system";
+import SvgIcon from "@mui/material/SvgIcon";
+
+const Wheel = (props) => {
+  return (
+    <SvgIcon {...props}>
+      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    </SvgIcon>
+  );
+};
 
 export const AOColourWheel = (cellColour) => {
   return (
@@ -8,6 +17,8 @@ export const AOColourWheel = (cellColour) => {
         height: "50px",
         backgroundColor: cellColour.cellColour,
       }}
-    ></Box>
+    >
+      <Wheel />
+    </Box>
   );
 };
