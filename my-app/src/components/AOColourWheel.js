e4974d5807ba69def8e2793ec044c0c0a182d209
evnwttn <evnwttn@gmail.com>
2022-04-21 10:41:32 -0400

    Update AOColourWheel.js

--- my-app/src/components/AOColourWheel.js
+++ my-app/src/components/AOColourWheel.js
@@ -1,25 +1,40 @@
 import SvgIcon from "@mui/material/SvgIcon";
+import { Box } from "@mui/system";
 import { palette } from "../assets/theme";
 
 const Wheel = (props) => {
   return (
     <>
-      {/* TOP LEFT // YELLOW */}
-      <SvgIcon
-        sx={{
-          transform: "rotate(270deg)",
-        }}
-      >
-        <svg color={palette.aoYellow}>
-          <path d="M5.95 11h13.988A8.023 8.023 0 0 0 13 4.062V2.049c4.714.475 8.476 4.237 8.95 8.951Z" />
-        </svg>
-      </SvgIcon>
-      {/* TOP RIGHT // BLUE */}
-      <SvgIcon>
-        <svg color={palette.aoBlue}>
-          <path d="M21.95,11L19.938,11C19.482,7.386 16.614,4.518 13,4.062L13,2.049C17.714,2.524 21.476,6.286 21.95,11Z" />
-        </svg>
-      </SvgIcon>
+      <Box>
+        {/* TOP LEFT // YELLOW */}
+        <SvgIcon
+          sx={{
+            backgroundColor: "black",
+            transform: "rotate(270deg)",
+          }}
+        >
+          <svg color={palette.aoYellow}>
+            <path d="M5.95 11h13.988A8.023 8.023 0 0 0 13 4.062V2.049c4.714.475 8.476 4.237 8.95 8.951Z" />
+          </svg>
+        </SvgIcon>
+        {/* TOP RIGHT // BLUE */}
+        <SvgIcon sx={{ backgroundColor: "black" }}>
+          <svg color={palette.aoBlue}>
+            <path d="M21.95,11L19.938,11C19.482,7.386 16.614,4.518 13,4.062L13,2.049C17.714,2.524 21.476,6.286 21.95,11Z" />
+          </svg>
+        </SvgIcon>
+      </Box>
+      <Box>
+        <SvgIcon
+          sx={{
+            transform: "rotate(180deg)",
+          }}
+        >
+          <svg color={palette.aoRed}>
+            <path d="M5.95 11h13.988A8.023 8.023 0 0 0 13 4.062V2.049c4.714.475 8.476 4.237 8.95 8.951Z" />
+          </svg>
+        </SvgIcon>
+      </Box>
     </>
   );
 };
