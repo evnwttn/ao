9ec566581aa6ccd7a1b7e4dfd513216999e96027
evnwttn <evnwttn@gmail.com>
2022-04-22 08:11:41 -0400

    Create AOSpoke.js

---
+++ my-app/src/components/AOSpoke.js
@@ -0,0 +1,21 @@
+import SvgIcon from "@mui/material/SvgIcon";
+import { palette } from "../assets/theme";
+
+const Spoke = (spoke) => {
+  return (
+    <SvgIcon
+      id={spoke.id}
+      viewBox="10 -10 24 24"
+      sx={{
+        height: "10vw",
+        width: "10vw",
+        transform: spoke.transform,
+        color: spoke.color,
+      }}
+    >
+      <svg>
+        <path d="M5.95 11h13.988A8.023 8.023 0 0 0 13 4.062V2.049c4.714.475 8.476 4.237 8.95 8.951Z" />
+      </svg>
+    </SvgIcon>
+  );
+};
