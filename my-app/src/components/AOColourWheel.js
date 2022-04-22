6f96a51ca018d741fcea2aac43bade74e97ce947
evnwttn <evnwttn@gmail.com>
2022-04-22 08:04:52 -0400

    Update AOColourWheel.js

--- my-app/src/components/AOColourWheel.js
+++ my-app/src/components/AOColourWheel.js
@@ -2,21 +2,9 @@ import SvgIcon from "@mui/material/SvgIcon";
 import { Box } from "@mui/system";
 import { palette } from "../assets/theme";
 
-// const wheelData = {
-//   topRow: [
-//     { key: "yellow", color: palette.aoYellow, rotate: "rotate(270deg)" },
-//     { key: "blue", color: palette.aoBlue },
-//   ],
-//   bottomRow: [
-//     { key: "grey", color: palette.aoGrey, rotate: "rotate(180deg)" },
-//     { key: "empty", color: "transparent", rotate: "rotate(90deg)" },
-//   ],
-// };
-
-const Spoke = (spoke, color, transform) => {
+const Spoke = (spoke) => {
   return (
     <SvgIcon
-      key={spoke.spoke}
       viewBox="10 -10 24 24"
       sx={{
         height: "10vw",
@@ -32,87 +20,17 @@ const Spoke = (spoke, color, transform) => {
   );
 };
 
-const Wheel = () => {
+export const AOColourWheel = () => {
   return (
     <>
       <Box>
-        <Spoke
-          spoke="yellow"
-          color={palette.aoYellow}
-          transform="rotate(270deg)"
-        />
+        <Spoke color={palette.aoYellow} transform="rotate(270deg)" />
+        <Spoke color={palette.aoBlue} transform="none" />
       </Box>
-      <Box></Box>
+      <Box>
+        <Spoke color={palette.aoGrey} transform="rotate(180deg)" />
+        <Spoke color="transparent" transform="rotate(90deg)" />
+      </Box>
     </>
   );
 };
-
-export const AOColourWheel = (cellColour) => {
-  return <Wheel />;
-};
-
-// const OGBuild = () => {
-//   return (
-//     <>
-//       <Box>
-//         <SvgIcon
-//           id="yellow"
-//           viewBox="10 -10 24 24"
-//           sx={{
-//             height: "10vw",
-//             width: "10vw",
-//             transform: "rotate(270deg)",
-//             color: palette.aoYellow,
-//           }}
-//         >
-//           <svg>
-//             <path d="M5.95 11h13.988A8.023 8.023 0 0 0 13 4.062V2.049c4.714.475 8.476 4.237 8.95 8.951Z" />
-//           </svg>
-//         </SvgIcon>
-//         <SvgIcon
-//           id="blue"
-//           viewBox="10 -10 24 24"
-//           sx={{
-//             color: palette.aoBlue,
-//             height: "10vw",
-//             width: "10vw",
-//           }}
-//         >
-//           <svg>
-//             <path d="M5.95,11L19.938,11C19.482,7.386 16.614,4.518 13,4.062L13,2.049C17.714,2.524 21.476,6.286 21.95,11Z" />
-//           </svg>
-//         </SvgIcon>
-//       </Box>
-//       <Box>
-//         <SvgIcon
-//           id="grey"
-//           viewBox="10 -10 24 24"
-//           sx={{
-//             transform: "rotate(180deg)",
-//             color: palette.aoGrey,
-//             height: "10vw",
-//             width: "10vw",
-//           }}
-//         >
-//           <svg>
-//             <path d="M5.95 11h13.988A8.023 8.023 0 0 0 13 4.062V2.049c4.714.475 8.476 4.237 8.95 8.951Z" />
-//           </svg>
-//         </SvgIcon>
-//         <SvgIcon
-//           id="transparent"
-//           viewBox="10 -10 24 24"
-//           sx={{
-//             transform: "rotate(90deg)",
-//             color: "transparent",
-//             height: "10vw",
-//             width: "10vw",
-//           }}
-//         >
-//           <svg>
-//             <path d="M5.95 11h13.988A8.023 8.023 0 0 0 13 4.062V2.049c4.714.475 8.476 4.237 8.95 8.951Z" />
-//           </svg>
-//         </SvgIcon>
-//       </Box>
-//     </>
-//   );
-// };
