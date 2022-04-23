1ea5e9d463de156ba02f4530dffe61144a366a9b
evnwttn <evnwttn@gmail.com>
2022-04-23 09:25:50 -0400

    Update AOSpoke.js

--- my-app/src/components/AOSpoke.js
+++ my-app/src/components/AOSpoke.js
@@ -5,16 +5,16 @@ export const AOSpoke = (spoke) => {
     <Box
       sx={{
         display: "inline-flex",
-        height: "11vw",
-        width: "11vw",
-        transform: spoke.transform,
+        svg: {
+          height: "11vw",
+          width: "11vw",
+          fill: spoke.color,
+          transform: spoke.transform,
+        },
       }}
     >
       <svg viewBox="10 -10 24 24">
-        <path
-          fill={spoke.color}
-          d="M5.95 11h13.988A8.023 8.023 0 0 0 13 4.062V2.049c4.714.475 8.476 4.237 8.95 8.951Z"
-        />
+        <path d="M5.95 11h13.988A8.023 8.023 0 0 0 13 4.062V2.049c4.714.475 8.476 4.237 8.95 8.951Z" />
       </svg>
     </Box>
   );
