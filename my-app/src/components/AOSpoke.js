600986a4a1ef6a649131660aba9b103fbf301cc3
evnwttn <evnwttn@gmail.com>
2022-04-29 16:05:57 -0230

    Update AOSpoke.js

--- my-app/src/components/AOSpoke.js
+++ my-app/src/components/AOSpoke.js
@@ -1,5 +1,6 @@
 import { Box } from "@mui/system";
 import { spokeSx } from "../assets/theme";
+import { quarterCircle } from "../assets/theme";
 
 export const AOSpoke = ({ color, transform, handleClick }) => {
   return (
@@ -10,11 +11,7 @@ export const AOSpoke = ({ color, transform, handleClick }) => {
         transform={transform}
         viewBox="10 -10 24 24"
       >
-        <path
-          onClick={() => handleClick(color)}
-          // RENDER SEMI CIRCLE
-          d="M5.95 11h13.988A8.023 8.023 0 0 0 13 4.062V2.049c4.714.475 8.476 4.237 8.95 8.951Z"
-        />
+        <path onClick={() => handleClick(color)} d={quarterCircle.svg} />
       </svg>
     </Box>
   );
