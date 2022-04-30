7e10db1332a090ea324a9e267621d322189124cf
evnwttn <evnwttn@gmail.com>
2022-04-30 20:32:41 -0230

    popoverradio

--- my-app/src/components/AOColourWheel.js
+++ my-app/src/components/AOColourWheel.js
@@ -1,35 +1,10 @@
-import { useState } from "react";
 import { Box } from "@mui/system";
-import { Radio } from "@mui/material";
 import { palette } from "../assets/theme";
 import { AOSpoke } from "./AOSpoke";
 
 export const AOColourWheel = ({ handleClick }) => {
-  const [selectedValue, setSelectedValue] = useState("1");
-
-  const handleChange = (event) => {
-    setSelectedValue(event.target.value);
-  };
-
   return (
     <>
-      <Box
-        sx={{
-          pointerEvents: "auto",
-          "&:hover": { opacity: "0.6", cursor: "pointer" },
-        }}
-      >
-        <Radio
-          checked={selectedValue === "1"}
-          onChange={handleChange}
-          value="1"
-        />
-        <Radio
-          checked={selectedValue === "2"}
-          onChange={handleChange}
-          value="2"
-        />
-      </Box>
       <Box>
         <AOSpoke
           color={palette.aoYellow}
---
+++ my-app/src/components/AOPopoverRadio.js
@@ -0,0 +1,31 @@
+import { Box, Radio } from "@mui/system";
+import { useState } from "react";
+
+export const AOPopoverRadio = () => {
+  const [selectedValue, setSelectedValue] = useState("1");
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+    console.log(selectedValue);
+  };
+
+  return (
+    <Box
+      sx={{
+        pointerEvents: "auto",
+        "&:hover": { opacity: "0.6", cursor: "pointer" },
+      }}
+    >
+      <Radio
+        checked={selectedValue === "1"}
+        onChange={handleChange}
+        value="1"
+      />
+      <Radio
+        checked={selectedValue === "2"}
+        onChange={handleChange}
+        value="2"
+      />
+    </Box>
+  );
+};
