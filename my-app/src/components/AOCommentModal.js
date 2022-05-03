767aa4cb7d46262683a21bcaa81ffb0a42b8a793
evnwttn <evnwttn@gmail.com>
2022-05-03 20:25:09 -0230

    update modals

--- my-app/src/components/AOCommentModal.js
+++ my-app/src/components/AOCommentModal.js
@@ -1,21 +1,13 @@
 import { Box } from "@mui/system";
 import { AOCommentContent } from "./modals/AOCommentContent";
-import { modalFontSx, palette } from "../assets/theme";
+import { modalFontSx, modalBoxStyling } from "../assets/theme";
 
 export const AOCommentModal = () => {
   return (
     <Box
       sx={{
         ...modalFontSx,
-        bgcolor: palette.aoGrey,
-        textAlign: "center",
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        width: "33%",
-        boxShadow: "0px 0px 50pt rbg(0 0 0 / 0.16)",
-        p: "2.75vw",
+        ...modalBoxStyling,
       }}
     >
       <AOCommentContent />
--- my-app/src/components/ModalText.js
+++ my-app/src/components/ModalText.js
@@ -3,22 +3,14 @@ import { NewModalContent } from "./modals/NewModalContent";
 import { LoadModalContent } from "./modals/LoadModalContent";
 import { ContactModalContent } from "./modals/ContactModalContent";
 import { AboutModalContent } from "./modals/AboutModalContent";
-import { palette, logoSx, modalFontSx } from "../assets/theme";
+import { palette, logoSx, modalFontSx, modalBoxStyling } from "../assets/theme";
 
 export const ModalText = ({ prompt }) => {
   return (
     <Box
       sx={{
         ...modalFontSx,
-        bgcolor: palette.aoGrey,
-        textAlign: "center",
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        width: "33%",
-        boxShadow: "0px 0px 50pt rbg(0 0 0 / 0.16)",
-        p: "2.75vw",
+        ...modalBoxStyling,
       }}
     >
       <Box
