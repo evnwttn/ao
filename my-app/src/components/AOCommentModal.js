28790a45c38b35f21647586c7a40a62884792e5b
evnwttn <evnwttn@gmail.com>
2022-05-03 20:52:27 -0230

    content in content modal in modal

--- my-app/src/components/AOCommentModal.js
+++ my-app/src/components/AOCommentModal.js
@@ -1,6 +1,7 @@
 import { Box } from "@mui/system";
+import { Divider } from "@mui/material";
 import { AOCommentContent } from "./modals/AOCommentContent";
-import { modalFontSx, modalBoxStyling } from "../assets/theme";
+import { modalFontSx, modalBoxStyling, palette } from "../assets/theme";
 
 export const AOCommentModal = ({ cell }) => {
   return (
@@ -10,6 +11,24 @@ export const AOCommentModal = ({ cell }) => {
         ...modalBoxStyling,
       }}
     >
+      <Box
+        sx={{
+          ...modalFontSx,
+          display: "inline-flex",
+          mb: 1,
+        }}
+      >
+        <Box sx={{ pr: 1, fontWeight: "bold", color: palette.aoRed }}>
+          {cell.track}
+        </Box>
+        <Divider
+          orientation="vertical"
+          flexItem
+          sx={{ color: palette.aoDivider }}
+        />
+        <Box sx={{ pl: 1 }}>{cell.parameter}</Box>
+      </Box>
+      <Divider variant="middle" sx={{ color: palette.aoDivider }} />
       <AOCommentContent cell={cell} />
     </Box>
   );
--- my-app/src/components/modals/AOCommentContent.js
+++ my-app/src/components/modals/AOCommentContent.js
@@ -1,29 +1,5 @@
 import { Box } from "@mui/system";
-import { Divider } from "@mui/material";
-import { palette, modalFontSx } from "../../assets/theme";
 
-export const AOCommentContent = ({ cell }) => {
-  return (
-    <>
-      <Box
-        sx={{
-          ...modalFontSx,
-          display: "inline-flex",
-          mb: 1,
-        }}
-      >
-        <Box sx={{ pr: 1, fontWeight: "bold", color: palette.aoRed }}>
-          {cell.track}
-        </Box>
-        <Divider
-          orientation="vertical"
-          flexItem
-          sx={{ color: palette.aoDivider }}
-        />
-        <Box sx={{ pl: 1 }}>{cell.parameter}</Box>
-      </Box>
-      <Divider variant="middle" sx={{ color: palette.aoDivider }} />
-      <Box sx={{ mt: 3 }}>Comments</Box>
-    </>
-  );
+export const AOCommentContent = () => {
+  return <Box sx={{ mt: 3 }}>Comments</Box>;
 };
