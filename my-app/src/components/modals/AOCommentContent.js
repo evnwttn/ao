8f9d625a1afc23e526830f66ac22dc3ad36b73d8
evnwttn <evnwttn@gmail.com>
2022-05-05 19:57:41 -0230

    comments gonna go hur prob

--- my-app/src/components/modals/AOCommentContent.js
+++ my-app/src/components/modals/AOCommentContent.js
@@ -1,5 +1,12 @@
 import { Box } from "@mui/system";
+import { TextField } from "@mui/material";
 
 export const AOCommentContent = () => {
-  return <Box sx={{ mt: 3 }}>Comments</Box>;
+  return (
+    <Box sx={{ mt: 3 }}>
+      <Box>
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+      </Box>
+    </Box>
+  );
 };
