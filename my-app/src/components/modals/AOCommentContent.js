7f7983164e60b44c63b22dd97d86f4b121ee1a69
evnwttn <evnwttn@gmail.com>
2022-05-05 20:12:39 -0230

    Update AOCommentContent.js

--- my-app/src/components/modals/AOCommentContent.js
+++ my-app/src/components/modals/AOCommentContent.js
@@ -1,11 +1,26 @@
 import { Box } from "@mui/system";
+import { palette } from "../../assets/theme";
 import { TextField } from "@mui/material";
+
+const inputProps = {
+  sx: {
+    padding: "1vw",
+    color: palette.aoBlack,
+    backgroundColor: palette.aoWhite,
+  },
+};
 
 export const AOCommentContent = ({ cell }) => {
   return (
-    <Box sx={{ mt: 3 }}>
+    <Box>
       <Box>
-        <TextField id={`${cell.track}.${cell.parameter}`} variant="filled" />
+        <TextField
+          id={`${cell.track}.${cell.parameter}`}
+          inputProps={inputProps}
+          sx={{ paddingTop: "0.5vw", width: "85%" }}
+          margin="normal"
+          variant="filled"
+        />
       </Box>
     </Box>
   );
