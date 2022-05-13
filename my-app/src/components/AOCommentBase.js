8b716da0ca20503d85f8a38391bed98897bb897f
evnwttn <evnwttn@gmail.com>
2022-05-13 15:46:12 -0230

    Update AOCommentBase.js

--- my-app/src/components/AOCommentBase.js
+++ my-app/src/components/AOCommentBase.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Modal } from "@mui/material";
 import { AOCommentModal } from "./AOCommentModal";
 
+// the fragment prevents a MUI related bug
+
 export const AOCommentBase = ({
   cell,
   open,
