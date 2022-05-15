e3efb311b9e52e7df335f356a2b912ab893ac331
evnwttn <evnwttn@gmail.com>
2022-05-15 17:47:58 -0230

    Update AOSpoke.js

--- my-app/src/components/AOSpoke.js
+++ my-app/src/components/AOSpoke.js
@@ -1,32 +1,33 @@
 import { Box } from "@mui/system";
-import { wheelSx } from "../assets/theme";
-import { svgShapes } from "../assets/theme";
+import { wheelSx, svgShapes, palette } from "../assets/theme";
 import { Tooltip } from "@mui/material/";
 
 export const AOSpoke = ({ color, transform, handleClick }) => {
   return (
-    <Box
-      sx={
-        color === "transparent"
-          ? { ...wheelSx.transparent }
-          : { ...wheelSx.spoke }
-      }
-    >
-      <svg
-        width="12vw"
-        fill={color}
-        onClick={() => {
-          handleClick(color);
-        }}
-        transform={transform}
-        viewBox="11 -11 24 24"
+    <Tooltip title="Delete">
+      <Box
+        sx={
+          color === "transparent"
+            ? { ...wheelSx.transparent }
+            : { ...wheelSx.spoke }
+        }
       >
-        {color === "transparent" ? (
-          <rect width="24" height="24" />
-        ) : (
-          <path d={svgShapes.quarterCircle} />
-        )}
-      </svg>
-    </Box>
+        <svg
+          width="12vw"
+          fill={color}
+          onClick={() => {
+            handleClick(color);
+          }}
+          transform={transform}
+          viewBox="11 -11 24 24"
+        >
+          {color === "transparent" ? (
+            <rect width="24" height="24" />
+          ) : (
+            <path d={svgShapes.quarterCircle} />
+          )}
+        </svg>
+      </Box>
+    </Tooltip>
   );
 };
