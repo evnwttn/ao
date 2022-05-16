28fa3859aa57325f35bbf1c7559283336c428f83
evnwttn <evnwttn@gmail.com>
2022-05-16 20:46:27 -0230

    semi not really works

--- my-app/src/components/AOSpoke.js
+++ my-app/src/components/AOSpoke.js
@@ -26,7 +26,18 @@ export const AOSpoke = ({
         transform={transform}
         viewBox="11 -11 24 24"
       >
-        <Tooltip title={toolTip} placement={placement}>
+        <Tooltip
+          PopperProps={{
+            sx: {
+              width: color === "transparent" && "9vw",
+              height: color === "transparent" && "1vw",
+              marginLeft: color === "transparent" && "-5vw !important",
+              marginBottom: color === "transparent" && "2.5vw !important",
+            },
+          }}
+          title={toolTip}
+          placement={placement}
+        >
           {color === "transparent" ? (
             <rect width="24" height="24" />
           ) : (
