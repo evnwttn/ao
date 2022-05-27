e2fcf1e4f37c194095b6f265a5837e2b631623e0
evnwttn <evnwttn@gmail.com>
2022-05-27 15:16:34 -0230

    Update NewSessionREDUX.js

--- my-app/src/components/modals/NewSessionREDUX.js
+++ my-app/src/components/modals/NewSessionREDUX.js
@@ -6,7 +6,7 @@ import { modalCenteredSx } from "../../assets/theme";
 
 export const NewSesh = () => {
   const [formNumber, setFormNumber] = useState(0);
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit } = useForm({
     defaultValues: {
       parameters: ["title"],
     },
@@ -20,34 +20,36 @@ export const NewSesh = () => {
 
   return (
     formNumber <= 1 && (
-      <Box
-        sx={{
-          ...modalCenteredSx,
-        }}
-        component="form"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <TextField
-          variant="standard"
-          helperText={
-            formNumber === 0
-              ? "who is this session for?"
-              : "enter a session title to continue"
-          }
-          margin="normal"
-          autoComplete="off"
-          {...(formNumber === 0 ? register("author") : register("id"))}
-        />
-        <Box>
-          <IconButton disableRipple type="submit">
-            <CheckCircleIcon
-              sx={{
-                my: 0.5,
-              }}
-            />
-          </IconButton>
+      <>
+        <Box
+          sx={{
+            ...modalCenteredSx,
+          }}
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <TextField
+            variant="standard"
+            helperText={
+              formNumber === 0
+                ? "who is this session for?"
+                : "enter a session title to continue"
+            }
+            margin="normal"
+            autoComplete="off"
+            {...(formNumber === 0 ? register("author") : register("id"))}
+          />
+          <Box>
+            <IconButton disableRipple type="submit">
+              <CheckCircleIcon
+                sx={{
+                  my: 0.5,
+                }}
+              />
+            </IconButton>
+          </Box>
         </Box>
-      </Box>
+      </>
     )
   );
 };
