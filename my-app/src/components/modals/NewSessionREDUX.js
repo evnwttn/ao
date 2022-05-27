ef8aa0c1b33b338b889b8753b76910a8efc41ae9
evnwttn <evnwttn@gmail.com>
2022-05-27 14:21:12 -0230

    Update NewSessionREDUX.js

--- my-app/src/components/modals/NewSessionREDUX.js
+++ my-app/src/components/modals/NewSessionREDUX.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 export const NewSesh = () => {
+  const [sessionId, setSessionId] = useState();
+  const [sessionData, setSessionData] = useState();
+  const [formCompleted, setFormCompleted] = useState(0);
+  //   const { register, handleSubmit, watch } = useForm({
+  //     defaultValues: {
+  //       id: "",
+  //       author: "",
+  //       tracks: [],
+  //       parameters: ["title"],
+  //     },
+  //   });
+
   return;
 };
