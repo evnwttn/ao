cbaabc56008854008f785e5498775b3f1cd68c83
evnwttn <evnwttn@gmail.com>
2022-05-28 14:51:38 -0230

    that's as tight as she gon get

--- my-app/src/components/modals/NewModalContentList.js
+++ my-app/src/components/modals/NewModalContentList.js
@@ -4,48 +4,44 @@ import { palette } from "../../assets/theme";
 import { modalCenteredSx } from "../../assets/theme";
 
 export const NewModalContentList = ({ data, input, onSubmit }) => {
-  return input === "tracks" ? (
+  return (
     <Box sx={{ ...modalCenteredSx, my: 1 }}>
       <List sx={{ width: "85%" }}>
-        {data.tracks &&
-          data.tracks.map((track, number) => {
-            return (
-              <ListItem
-                key={`${track.title}.${number}`}
-                sx={{ bgcolor: palette.aoBlack }}
-                secondaryAction={
-                  <IconButton onClick={() => onSubmit(track)}>
-                    <ClearIcon />
-                  </IconButton>
-                }
-              >
-                <ListItemText primary={track.title} />
-              </ListItem>
-            );
-          })}
-      </List>
-    </Box>
-  ) : (
-    <Box sx={{ ...modalCenteredSx, my: 1 }}>
-      <List sx={{ width: "85%" }}>
-        {data.parameters &&
-          data.parameters.map((parameter, number) => {
-            return (
-              parameter !== "title" && (
+        {input === "tracks"
+          ? data.tracks &&
+            data.tracks.map((track, number) => {
+              return (
                 <ListItem
-                  key={`${parameter}.${number}`}
+                  key={`${track.title}.${number}`}
                   sx={{ bgcolor: palette.aoBlack }}
                   secondaryAction={
-                    <IconButton onClick={() => onSubmit(parameter)}>
+                    <IconButton onClick={() => onSubmit(track)}>
                       <ClearIcon />
                     </IconButton>
                   }
                 >
-                  <ListItemText primary={parameter} />
+                  <ListItemText primary={track.title} />
                 </ListItem>
-              )
-            );
-          })}
+              );
+            })
+          : data.parameters &&
+            data.parameters.map((parameter, number) => {
+              return (
+                parameter !== "title" && (
+                  <ListItem
+                    key={`${parameter}.${number}`}
+                    sx={{ bgcolor: palette.aoBlack }}
+                    secondaryAction={
+                      <IconButton onClick={() => onSubmit(parameter)}>
+                        <ClearIcon />
+                      </IconButton>
+                    }
+                  >
+                    <ListItemText primary={parameter} />
+                  </ListItem>
+                )
+              );
+            })}
       </List>
     </Box>
   );
