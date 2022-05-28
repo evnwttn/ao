4fe6871f9a9d0188c3ccb477e2de57176ab5994b
evnwttn <evnwttn@gmail.com>
2022-05-28 14:36:36 -0230

    Update NewModalContentList.js

--- my-app/src/components/modals/NewModalContentList.js
+++ my-app/src/components/modals/NewModalContentList.js
@@ -1,11 +1,10 @@
-import { useState } from "react";
 import { Box, IconButton, List, ListItem, ListItemText } from "@mui/material";
 import ClearIcon from "@mui/icons-material/Clear";
 import { palette } from "../../assets/theme";
 import { modalCenteredSx } from "../../assets/theme";
 
 export const NewModalContentList = ({ data, input, onSubmit }) => {
-  return (
+  return input === "tracks" ? (
     <Box sx={{ ...modalCenteredSx, my: 1 }}>
       <List sx={{ width: "85%" }}>
         {data.tracks &&
@@ -21,6 +20,27 @@ export const NewModalContentList = ({ data, input, onSubmit }) => {
                 }
               >
                 <ListItemText primary={track.title} />
+              </ListItem>
+            );
+          })}
+      </List>
+    </Box>
+  ) : (
+    <Box sx={{ ...modalCenteredSx, my: 1 }}>
+      <List sx={{ width: "85%" }}>
+        {data.parameters &&
+          data.parameters.map((parameter, number) => {
+            return (
+              <ListItem
+                key={`${parameter}.${number}`}
+                sx={{ bgcolor: palette.aoBlack }}
+                secondaryAction={
+                  <IconButton onClick={() => onSubmit(parameter)}>
+                    <ClearIcon />
+                  </IconButton>
+                }
+              >
+                <ListItemText primary={parameter} />
               </ListItem>
             );
           })}
