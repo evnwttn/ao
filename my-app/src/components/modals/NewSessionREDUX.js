b9ae00d9a11802bbf2f43147dee69772ae9c9ce8
evnwttn <evnwttn@gmail.com>
2022-05-28 11:48:44 -0230

    Update NewSessionREDUX.js

--- my-app/src/components/modals/NewSessionREDUX.js
+++ my-app/src/components/modals/NewSessionREDUX.js
@@ -15,41 +15,38 @@ export const NewSesh = () => {
   const onSubmit = (data) => {
     data.author && setFormNumber(formNumber + 1);
     data.id && setFormNumber(formNumber + 1);
-    console.log(data);
   };
 
-  return (
-    formNumber <= 1 && (
-      <>
-        <Box
-          sx={{
-            ...modalCenteredSx,
-          }}
-          component="form"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <TextField
-            variant="standard"
-            helperText={
-              formNumber === 0
-                ? "who is this session for?"
-                : "enter a session title to continue"
-            }
-            margin="normal"
-            autoComplete="off"
-            {...(formNumber === 0 ? register("author") : register("id"))}
+  return formNumber <= 1 ? (
+    <Box
+      sx={{
+        ...modalCenteredSx,
+      }}
+      component="form"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <TextField
+        variant="standard"
+        helperText={
+          formNumber === 0
+            ? "enter artist name to continue"
+            : "enter a session title to continue"
+        }
+        margin="normal"
+        autoComplete="off"
+        {...(formNumber === 0 ? register("author") : register("id"))}
+      />
+      <Box>
+        <IconButton disableRipple type="submit">
+          <CheckCircleIcon
+            sx={{
+              my: 0.5,
+            }}
           />
-          <Box>
-            <IconButton disableRipple type="submit">
-              <CheckCircleIcon
-                sx={{
-                  my: 0.5,
-                }}
-              />
-            </IconButton>
-          </Box>
-        </Box>
-      </>
-    )
+        </IconButton>
+      </Box>
+    </Box>
+  ) : (
+    <Box>Ayyy lol</Box>
   );
 };
