b2a0780234cadf8871408f0768a602b0c1197efd
evnwttn <evnwttn@gmail.com>
2022-05-28 14:23:06 -0230

    Update NewModalContentList.js

--- my-app/src/components/modals/NewModalContentList.js
+++ my-app/src/components/modals/NewModalContentList.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Box, IconButton, List, ListItem, ListItemText } from "@mui/material";
 import ClearIcon from "@mui/icons-material/Clear";
 import { palette } from "../../assets/theme";
@@ -8,10 +9,10 @@ export const NewModalContentList = ({ data, input, onSubmit }) => {
     <Box sx={{ ...modalCenteredSx, my: 1 }}>
       <List sx={{ width: "85%" }}>
         {data.tracks &&
-          data.tracks.map((track) => {
+          data.tracks.map((track, number) => {
             return (
               <ListItem
-                key={track.title}
+                key={`${track.title}.${number}`}
                 sx={{ bgcolor: palette.aoBlack }}
                 secondaryAction={
                   <IconButton onClick={() => onSubmit(track)}>
@@ -27,44 +28,3 @@ export const NewModalContentList = ({ data, input, onSubmit }) => {
     </Box>
   );
 };
-{
-  /* <Box sx={{ ...modalCenteredSx, my: 1 }}>
-      <List sx={{ width: "85%" }}>
-        {target === `tracks`
-          ? input.tracks &&
-            input.tracks.map((track) => {
-              return (
-                <ListItem
-                  key={track}
-                  sx={{ bgcolor: palette.aoBlack }}
-                  secondaryAction={
-                    <IconButton onClick={() => onSubmit(track)}>
-                      <ClearIcon />
-                    </IconButton>
-                  }
-                >
-                  <ListItemText primary={track} />
-                </ListItem>
-              );
-            })
-          : input.parameters &&
-            input.parameters.map((parameter) => {
-              return (
-                parameter !== "title" && (
-                  <ListItem
-                    key={parameter}
-                    sx={{ bgcolor: palette.aoBlack }}
-                    secondaryAction={
-                      <IconButton onClick={() => onSubmit(parameter)}>
-                        <ClearIcon />
-                      </IconButton>
-                    }
-                  >
-                    <ListItemText primary={parameter} />
-                  </ListItem>
-                )
-              );
-            })}
-      </List>
-    </Box> */
-}
