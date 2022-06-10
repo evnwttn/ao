258b22c188209ea286a560c451c2842645db0628
evnwttn <evnwttn@gmail.com>
2022-06-10 16:34:19 -0230

    temp

--- my-app/src/components/modals/AONavAboutContent.js
+++ my-app/src/components/modals/AONavAboutContent.js
@@ -1,4 +1,5 @@
 import { Box } from "@mui/material";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 import { modalCenteredSx, palette } from "../../assets/theme";
 
 export const AONavAboutContent = () => {
@@ -9,10 +10,11 @@ export const AONavAboutContent = () => {
       }}
     >
       YOU MAY CLICK ON A CELL TO BEGIN.
-      <Box sx={{ mt: 2 }}>
+      <Box sx={{ my: 2 }}>
         <b>COLOURWHEEL</b>
         <Box
           sx={{
+            my: "0.125vw",
             b: {
               color: palette.aoYellow,
             },
@@ -22,6 +24,8 @@ export const AONavAboutContent = () => {
         </Box>
         <Box
           sx={{
+            my: "0.125vw",
+
             b: {
               color: palette.aoBlue,
             },
@@ -31,6 +35,7 @@ export const AONavAboutContent = () => {
         </Box>
         <Box
           sx={{
+            my: "0.125vw",
             b: {
               textShadow: `0px 0px 10px rgb(243, 239, 224, 0.75)`,
               color: palette.aoBlack,
@@ -39,8 +44,12 @@ export const AONavAboutContent = () => {
         >
           <b>The Bottom Left</b> indicates that a task has been set.
         </Box>{" "}
-        <Box>
+        <Box sx={{ my: "0.125vw" }}>
           <b>The Bottom Right</b> opens the comment window.
+        </Box>
+        <Box sx={{ my: "0.125vw" }}>
+          <RemoveCircleOutlineIcon sx={{ fontSize: "1.4vw" }} />
+          removes a task from the grid.
         </Box>
       </Box>
     </Box>
