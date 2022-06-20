58594b32c2fbc0a3ee1e4e52e137be34dd288aea
evnwttn <evnwttn@gmail.com>
2022-06-20 20:33:17 -0230

    Update AOColorWheel.js

--- my-app/src/components/AOColorWheel.js
+++ my-app/src/components/AOColorWheel.js
@@ -1,6 +1,6 @@
 import { Box } from "@mui/system";
 import { AOSpoke } from "./AOSpoke";
-import { palette } from "../assets/theme";
+import { palette, svgShapes } from "../assets/theme";
 
 export const AOColorWheel = ({ handleClick, openComment }) => {
   return (
@@ -8,12 +8,14 @@ export const AOColorWheel = ({ handleClick, openComment }) => {
       <Box>
         <AOSpoke
           color={palette.aoYellow}
+          shape={svgShapes.topLeft}
           handleClick={(color) => handleClick(color)}
           toolTip="In Progress"
           placement="left-start"
         />
         <AOSpoke
           color={palette.aoBlue}
+          shape={svgShapes.topRight}
           handleClick={(color) => handleClick(color)}
           toolTip="Completed"
           placement="right-start"
@@ -22,12 +24,14 @@ export const AOColorWheel = ({ handleClick, openComment }) => {
       <Box>
         <AOSpoke
           color={palette.aoGrey}
+          shape={svgShapes.bottomLeft}
           handleClick={(color) => handleClick(color)}
           toolTip="Set Task"
           placement="left-end"
         />
         <AOSpoke
           color={palette.aoBlack}
+          shape={svgShapes.bottomRight}
           toolTip="Comments"
           placement="right-end"
           handleClick={openComment}
