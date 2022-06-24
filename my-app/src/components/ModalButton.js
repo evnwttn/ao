ae4d7eda9ee49f7be968a1b346ca75d3d59c6560
evnwttn <evnwttn@gmail.com>
2022-06-24 14:05:57 -0230

    Update ModalButton.js

--- my-app/src/components/ModalButton.js
+++ my-app/src/components/ModalButton.js
@@ -2,18 +2,16 @@ import { Button } from "@mui/material";
 
 export const ModalButton = ({ text, handleOpen }) => {
   return (
-    <>
-      <Button
-        disableRipple
-        key={text}
-        onClick={() => handleOpen(text)}
-        variant="text"
-        color={
-          text === "New" ? "primary" : text === "Load" ? "secondary" : "inherit"
-        }
-      >
-        {text}
-      </Button>
-    </>
+    <Button
+      disableRipple
+      key={text}
+      onClick={() => handleOpen(text)}
+      variant="text"
+      color={
+        text === "New" ? "primary" : text === "Load" ? "secondary" : "inherit"
+      }
+    >
+      {text}
+    </Button>
   );
 };
