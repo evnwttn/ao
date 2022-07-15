81969333b10814eb13c57cdc6571636844c5d561
evnwttn <evnwttn@gmail.com>
2022-07-15 15:48:34 -0230

    Update ParameterList.js

--- my-app/src/components/modals/ParameterList.js
+++ my-app/src/components/modals/ParameterList.js
@@ -1,19 +1,25 @@
 import { Box, Button } from "@mui/material";
 
 export const ParameterList = () => {
+  const parameters = [
+    "Vocals",
+    "Bass",
+    "Rhythm Guitar",
+    "Lead Guitar",
+    "Drums",
+    "Piano",
+    "Synth",
+    "Strings",
+    "FX",
+    "Mix",
+    "Master",
+  ];
+
   return (
-    <Box sx={{}}>
-      <Button variant="outlined">Vocals</Button>
-      <Button variant="outlined">Bass</Button>
-      <Button variant="outlined">Rhythm Guitar</Button>
-      <Button variant="outlined">Lead Guitar</Button>
-      <Button variant="outlined">Drums</Button>
-      <Button variant="outlined">Piano</Button>
-      <Button variant="outlined">Synth</Button>
-      <Button variant="outlined">Strings</Button>
-      <Button variant="outlined">FX</Button>
-      <Button variant="outlined">Mix</Button>
-      <Button variant="outlined">Master</Button>
+    <Box>
+      {parameters.map((parameter) => {
+        return <Button variant="outlined">{parameter}</Button>;
+      })}
     </Box>
   );
 };
