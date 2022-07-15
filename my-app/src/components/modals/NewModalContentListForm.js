73465d50c6979a5e9b1857fb8aba22175c6c17ad
evnwttn <evnwttn@gmail.com>
2022-07-15 10:58:16 -0230

    Update NewModalContentListForm.js

--- my-app/src/components/modals/NewModalContentListForm.js
+++ my-app/src/components/modals/NewModalContentListForm.js
@@ -4,9 +4,7 @@ import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { NewModalContentList } from "./NewModalContentList";
 import { modalCenteredSx, newSessionModal } from "../../assets/theme";
 
-// separate track / parameter forms
 // add in buttons for default options in parameter
-// rework the wording here
 // move submit to below list
 
 export const NewModalContentListForm = ({
@@ -29,7 +27,7 @@ export const NewModalContentListForm = ({
       >
         <Box sx={newSessionModal.text}>
           Enter the&nbsp;
-          {formPrompt === 2 ? <b>track titles</b> : <b>session parameters</b>}
+          {formPrompt === 2 ? <b>track titles</b> : <b>session elements</b>}
           &nbsp;you wish to include in the {sessionData.id} session.
           {formPrompt === 3 && "Buttons be going here"}
         </Box>
