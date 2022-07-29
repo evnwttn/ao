627a984bae09e13c3324a0717f472375d82f3fe7
evnwttn <evnwttn@gmail.com>
2022-07-29 15:49:55 -0230

    cleanup

--- my-app/src/app.ts
+++ my-app/src/app.ts
@@ -13,23 +13,10 @@ const app = express.default();
 
 // Register middlewares
 app.use(logger);
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // Register route handlers
 app.get("/contact", ContactRoute.handler);
 
 // Start our server and listen on port 3005
 app.listen(3005, () => console.log("holla"));
-
-//POST localhost:3005/contact
-
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// Route that receives a POST request to /sms
-app.post('/contact', function (req, res) {
-  res.header("Access-Control-Allow-Origin", "*");
-  const body = req.body.Body
-  console.log(body);
-  // res.set('Content-Type', 'text/plain')
-  // res.send(`You sent: ${body} to Express`)
-})
-
--- my-app/src/routes/contact.ts
+++ my-app/src/routes/contact.ts
@@ -1,4 +1,8 @@
 //@ts-ignore
-export function handler(req) {
-    console.log(req);
+export function handler(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    console.log('listening');
+
+    // const body = req.body.Body
+    // console.log(body);
 }
