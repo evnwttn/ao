63a94092263640ae8f64c787d39e5b27f8226357
evnwttn <evnwttn@gmail.com>
2022-08-01 19:34:04 -0230

    backend update

--- my-app/src/app.ts
+++ my-app/src/app.ts
@@ -17,7 +17,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.options("/contact", CORS.handler);
 
 // Register route handlers
-app.post("/contact", ContactRoute.handler);
+// app.post("/contact", ContactRoute.handler);
+
+app.post('/contact', (req, res) => {
+	res	 			//You need to use res to respond to the request (req)
+	.status(200)	//Setting the response status to 200 means that the request is OK
+	.json({ message: 'this should work' }); //Return an object (this is what your frontend should receive)
+});
 
 // Start our server and listen on port 3005
 app.listen(3005, () => console.log("server listening @ 3005"));
