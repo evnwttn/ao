a5b2f1d469cd8422804adbfad739a927f169721b
evnwttn <evnwttn@gmail.com>
2022-08-11 21:32:18 -0230

    remove old middleware

--- my-app/src/middleware/cors.ts
+++
@@ -1,4 +0,0 @@
-//@ts-ignore
-export function corsHandler(req, res, next) {   
-    res.header('Access-Control-Allow-Origin', '*');
-}
--- my-app/src/middleware/logger.ts
+++
@@ -1,7 +0,0 @@
-import { Request } from "express";
-
-//@ts-ignore
-export function logger(req: Request, res, next) {
-    console.log(`[REQUEST] ${req.path}`);
-    next();
-}
