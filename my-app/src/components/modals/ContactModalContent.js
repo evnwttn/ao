7df03e40975c0ac054d456b86bf26fee09b8e848
evnwttn <evnwttn@gmail.com>
2022-08-12 14:29:46 -0230

    Update ContactModalContent.js

--- my-app/src/components/modals/ContactModalContent.js
+++ my-app/src/components/modals/ContactModalContent.js
@@ -6,20 +6,11 @@ import { modalCenteredSx } from "../../assets/theme";
 
 export const ContactModalContent = () => {
   const { register, handleSubmit } = useForm();
-  // const onSubmit = (data) => {
-  //   console.log(`sending...`);
-  //   axios
-  //     .post("http://localhost:3005/contact", data)
-  //     .then((_data) => console.log(JSON.stringify(_data)));
-  // };
-
   const onSubmit = (data) => {
     console.log(`sending...`);
     axios
-      .post("/contact", data)
-      .then(function (response) {
-        console.log(response.data);
-      })
+      .post("http://localhost:3005/contact", data)
+      .then((_data) => console.log(JSON.stringify(_data)))
       .catch(function (error) {
         console.log(error);
       });
