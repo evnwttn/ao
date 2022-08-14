a4ff477970a7e28a85c5eb88e184be326f900a38
evnwttn <evnwttn@gmail.com>
2022-08-14 21:09:25 -0230

    Update ContactModalContent.js

--- my-app/src/components/modals/ContactModalContent.js
+++ my-app/src/components/modals/ContactModalContent.js
@@ -1,4 +1,3 @@
-// import { useForm } from "react-hook-form";
 import { useState, useRef, useEffect } from "react";
 import * as axios from "axios";
 import { TextField, IconButton, Box } from "@mui/material";
@@ -68,57 +67,3 @@ export const ContactModalContent = () => {
     </Box>
   );
 };
-
-// export const ContactModalContent = () => {
-//   const { register, handleSubmit } = useForm();
-
-// const onSubmit = (data) => {
-//   console.log(`sending...`);
-//   axios
-//     .post("http://localhost:5000/contact", {
-//       data,
-//     })
-//     .then((_data) => console.log(JSON.stringify(_data)))
-//     .catch(function (error) {
-//       console.log(error);
-//     });
-// };
-
-//   return (
-//     <Box
-//       component="form"
-//       sx={modalCenteredSx}
-//       onSubmit={handleSubmit(onSubmit)}
-//     >
-//       <TextField
-//         placeholder="Name"
-//         variant="standard"
-//         margin="normal"
-//         sx={{ width: "28vw" }}
-//         {...register("Name", {})}
-//       />
-//       <TextField
-//         placeholder="Email Address"
-//         variant="standard"
-//         margin="normal"
-//         sx={{ width: "28vw" }}
-//         {...register("Email Address", {})}
-//       />
-//       <TextField
-//         placeholder="Message"
-//         variant="standard"
-//         margin="normal"
-//         sx={{ width: "28vw" }}
-//         {...register("Message", {})}
-//       />
-//       <IconButton disableRipple sx={{ cursor: "default" }} type="submit">
-//         <SendIcon
-//           sx={{
-//             ml: "25vw",
-//             mt: "1vh",
-//           }}
-//         />
-//       </IconButton>
-//     </Box>
-//   );
-// };
