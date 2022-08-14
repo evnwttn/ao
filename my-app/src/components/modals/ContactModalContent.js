83ad965eecedef6a0881b1dd13a5640e3df9e24b
evnwttn <evnwttn@gmail.com>
2022-08-14 20:35:46 -0230

    contact form rewrite

--- my-app/src/components/modals/ContactModalContent.js
+++ my-app/src/components/modals/ContactModalContent.js
@@ -1,23 +1,15 @@
-import { useForm } from "react-hook-form";
+// import { useForm } from "react-hook-form";
+import { useState, useEffect } from "react";
 import * as axios from "axios";
 import { TextField, IconButton, Box } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import { modalCenteredSx } from "../../assets/theme";
 
 export const ContactModalContent = () => {
-  const { register, handleSubmit } = useForm();
-
-  const onSubmit = (data) => {
-    console.log(`sending...`);
-    axios
-      .post("http://localhost:5000/contact", {
-        data,
-      })
-      .then((_data) => console.log(JSON.stringify(_data)))
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
+  const [formData, setFormData] = useState([]);
+  useEffect(() => {
+    console.log(formData);
+  }, [formData]);
 
   return (
     <Box
@@ -57,3 +49,57 @@ export const ContactModalContent = () => {
     </Box>
   );
 };
+
+// export const ContactModalContent = () => {
+//   const { register, handleSubmit } = useForm();
+
+//   const onSubmit = (data) => {
+//     console.log(`sending...`);
+//     axios
+//       .post("http://localhost:5000/contact", {
+//         data,
+//       })
+//       .then((_data) => console.log(JSON.stringify(_data)))
+//       .catch(function (error) {
+//         console.log(error);
+//       });
+//   };
+
+//   return (
+//     <Box
+//       component="form"
+//       sx={modalCenteredSx}
+//       onSubmit={handleSubmit(onSubmit)}
+//     >
+//       <TextField
+//         placeholder="Name"
+//         variant="standard"
+//         margin="normal"
+//         sx={{ width: "28vw" }}
+//         {...register("Name", {})}
+//       />
+//       <TextField
+//         placeholder="Email Address"
+//         variant="standard"
+//         margin="normal"
+//         sx={{ width: "28vw" }}
+//         {...register("Email Address", {})}
+//       />
+//       <TextField
+//         placeholder="Message"
+//         variant="standard"
+//         margin="normal"
+//         sx={{ width: "28vw" }}
+//         {...register("Message", {})}
+//       />
+//       <IconButton disableRipple sx={{ cursor: "default" }} type="submit">
+//         <SendIcon
+//           sx={{
+//             ml: "25vw",
+//             mt: "1vh",
+//           }}
+//         />
+//       </IconButton>
+//     </Box>
+//   );
+// };
