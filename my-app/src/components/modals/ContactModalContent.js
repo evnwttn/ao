909f5be7e7e9a46b6c1c6baac85bd7d8019f837c
evnwttn <evnwttn@gmail.com>
2022-08-14 19:06:08 -0230

    prod build

--- my-app/src/components/modals/ContactModalContent.js
+++ my-app/src/components/modals/ContactModalContent.js
@@ -12,20 +12,24 @@ export const ContactModalContent = () => {
       .post("http://localhost:3005/contact", data)
       .then((_data) => console.log(JSON.stringify(_data)))
       .catch(function (error) {
-        console.log(error);
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
       });
   };
-
-  // const onSubmit = (data) => {
-  //   console.log(`sending...`);
-  //   let xhr = new XMLHttpRequest();
-  //   xhr.open("POST", "http://localhost:3005/contact", true);
-  //   xhr.setRequestHeader(
-  //     "Content-Type",
-  //     "application/x-www-form-urlencoded; charset=UTF-8"
-  //   );
-  //   xhr.send(data);
-  // };
 
   return (
     <Box
