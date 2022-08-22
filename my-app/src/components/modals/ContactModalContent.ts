7813cfceab1a06cb56553d6d0f78b89d586ad759
evnwttn <evnwttn@gmail.com>
2022-08-22 21:04:30 -0230

    Update ContactModalContent.ts

---
+++ my-app/src/components/modals/ContactModalContent.ts
@@ -0,0 +1,4 @@
+import { useState, useRef, useEffect } from "react";
+import { TextField, IconButton, Box, CircularProgress } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import { palette } from "../../assets/theme.js";
