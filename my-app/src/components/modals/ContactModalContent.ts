d2b27e23c7d9bad3e7eb0091131804a33f03b11b
evnwttn <evnwttn@gmail.com>
2022-08-22 21:23:31 -0230

    Update ContactModalContent.ts

--- my-app/src/components/modals/ContactModalContent.ts
+++ my-app/src/components/modals/ContactModalContent.ts
@@ -1,4 +1,5 @@
 import { useState, useRef, useEffect } from "react";
 import { TextField, IconButton, Box, CircularProgress } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
-import { palette } from "../../assets/theme";
+import { palette, modalCenteredSx, circularProgressSx } from "../../assets/theme";
+import axios = require("axios");
