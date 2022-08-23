eda21c563c05377b64f0e4962e1246829d784431
evnwttn <evnwttn@gmail.com>
2022-08-23 00:05:34 +0000

    Deploying to gh-pages from @ evnwttn/ao@128113c2aa6ce33556862183490e28ab93067f3c 🚀

--- components/modals/ContactModalContent.js
+++ components/modals/ContactModalContent.js
@@ -5,8 +5,8 @@ const react_1 = require("react");
 const ContantModalContentTS = () => {
     const [formData, setFormData] = (0, react_1.useState)({});
     const [formLoading, setFormLoading] = (0, react_1.useState)(false);
-    const nameField = (0, react_1.useRef)();
-    const emailField = (0, react_1.useRef)();
-    const messageField = (0, react_1.useRef)();
+    const nameField = (0, react_1.useRef)(null);
+    const emailField = (0, react_1.useRef)(null);
+    const messageField = (0, react_1.useRef)(null);
 };
 exports.ContantModalContentTS = ContantModalContentTS;
