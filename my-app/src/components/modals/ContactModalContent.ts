dcc407deb726158f54aaff31f73094230dd7ae13
evnwttn <evnwttn@gmail.com>
2022-08-24 19:49:35 -0230

    Update ContactModalContent.ts

--- my-app/src/components/modals/ContactModalContent.ts
+++ my-app/src/components/modals/ContactModalContent.ts
@@ -2,12 +2,27 @@ import { useState, useRef, useEffect } from "react";
 import { TextField, IconButton, Box, CircularProgress } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import { palette, modalCenteredSx, circularProgressSx } from "../../assets/theme";
-import axios = require("axios");
 
 export const ContantModalContentTS = () => {
+    const axios = require("axios").default;
     const [formData, setFormData] = useState<FormData>({} as FormData);
     const [formLoading, setFormLoading] = useState<Boolean>(false);
     const nameField = useRef<HTMLDivElement>(null);
     const emailField = useRef<HTMLDivElement>(null);
     const messageField = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (formData) {
+      setFormLoading(true);
+      axios
+        .post("http://localhost:5000/contact", {
+          ...formData,
+        })
+        .then((_data: any) => console.log(JSON.stringify(_data)))
+        .catch(function (error: any) {
+          console.log(error);
+        });
+    }
+  }, [formData, axios]);
+
 }
