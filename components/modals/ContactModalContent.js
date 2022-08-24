9352abda738b6c9a912de14fa1895d03eeb8cb3e
evnwttn <evnwttn@gmail.com>
2022-08-24 22:20:51 +0000

    Deploying to gh-pages from @ evnwttn/ao@dcc407deb726158f54aaff31f73094230dd7ae13 🚀

--- components/modals/ContactModalContent.js
+++ components/modals/ContactModalContent.js
@@ -3,10 +3,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ContantModalContentTS = void 0;
 const react_1 = require("react");
 const ContantModalContentTS = () => {
+    const axios = require("axios").default;
     const [formData, setFormData] = (0, react_1.useState)({});
     const [formLoading, setFormLoading] = (0, react_1.useState)(false);
     const nameField = (0, react_1.useRef)(null);
     const emailField = (0, react_1.useRef)(null);
     const messageField = (0, react_1.useRef)(null);
+    (0, react_1.useEffect)(() => {
+        if (formData) {
+            setFormLoading(true);
+            axios
+                .post("http://localhost:5000/contact", Object.assign({}, formData))
+                .then((_data) => console.log(JSON.stringify(_data)))
+                .catch(function (error) {
+                console.log(error);
+            });
+        }
+    }, [formData, axios]);
 };
 exports.ContantModalContentTS = ContantModalContentTS;
