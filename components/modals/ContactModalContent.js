b867dce0a88a5eabff1d569501ddb2433e2e757d
evnwttn <evnwttn@gmail.com>
2022-08-25 00:21:41 +0000

    Deploying to gh-pages from @ evnwttn/ao@ecc1a2a1c5f186843984f99c4712dfe9a64a4283 🚀

--- components/modals/ContactModalContent.js
+++ components/modals/ContactModalContent.js
@@ -22,14 +22,23 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ContantModalContentTS = void 0;
 const React = __importStar(require("react"));
 const react_1 = require("react");
 const material_1 = require("@mui/material");
+const Send_1 = __importDefault(require("@mui/icons-material/Send"));
+const theme_1 = require("../../assets/theme");
 const ContantModalContentTS = () => {
     const axios = require("axios").default;
-    const [formData, setFormData] = (0, react_1.useState)({});
+    const [formData, setFormData] = (0, react_1.useState)({
+        name: '',
+        email: '',
+        message: '',
+    });
     const [formLoading, setFormLoading] = (0, react_1.useState)(false);
     const nameField = (0, react_1.useRef)(null);
     const emailField = (0, react_1.useRef)(null);
@@ -52,6 +61,21 @@ const ContantModalContentTS = () => {
             }
         }, 2250);
     }, [formLoading]);
-    return (React.createElement(material_1.Box, null));
+    return (React.createElement(material_1.Box, { sx: theme_1.modalCenteredSx },
+        React.createElement(material_1.TextField, { placeholder: "Name", inputRef: nameField, variant: "standard", margin: "normal", sx: { width: "28vw" } }),
+        React.createElement(material_1.TextField, { placeholder: "Email Address", inputRef: emailField, variant: "standard", margin: "normal", sx: { width: "28vw" } }),
+        React.createElement(material_1.TextField, { placeholder: "Message", inputRef: messageField, variant: "standard", margin: "normal", sx: { width: "28vw" } }),
+        React.createElement(material_1.IconButton, { disableRipple: true, sx: { cursor: "default" }, onClick: () => {
+                var _a, _b, _c;
+                return setFormData({
+                    name: (_a = nameField === null || nameField === void 0 ? void 0 : nameField.current) === null || _a === void 0 ? void 0 : _a.value,
+                    email: (_b = emailField === null || emailField === void 0 ? void 0 : emailField.current) === null || _b === void 0 ? void 0 : _b.value,
+                    message: (_c = messageField === null || messageField === void 0 ? void 0 : messageField.current) === null || _c === void 0 ? void 0 : _c.value,
+                });
+            } },
+            React.createElement(material_1.CircularProgress, { size: "2.33vw", sx: Object.assign(Object.assign({}, theme_1.circularProgressSx), { color: formLoading === true ? theme_1.palette.aoBlue : theme_1.palette.aoGrey }) }),
+            React.createElement(Send_1.default, { sx: {
+                    mt: "1vh",
+                } }))));
 };
 exports.ContantModalContentTS = ContantModalContentTS;
