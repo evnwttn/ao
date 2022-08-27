f20c2699191e1bc8c891f19d21b3b0cd22d958fb
evnwttn <evnwttn@gmail.com>
2022-08-27 13:06:01 -0230

    Update NewModal.tsx

--- my-app/src/components/modals/NewModal.tsx
+++ my-app/src/components/modals/NewModal.tsx
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState,  useRef, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { NewModalContentTextfieldForm } from "./NewModalContentTextfieldForm";
 import { NewModalContentListForm } from "./NewModalContentListForm";
 
 export const NewModalContent = () => {
-    // const axios = require("axios").default;
+    const axios = require("axios").default;
     // const [sessionData, setSessionData] = useState();
-    // const [startNewSession, setStartNewSession] = useState(false);
-    // const [formPrompt, setFormPrompt] = useState(0);
-    // const [triggerSubmit, setTriggerSubmit] = useState(0);
-    // const [inputArray, setInputArray] = useState([]);
+    const [startNewSession, setStartNewSession] = useState<Boolean>(false);
+    const [formPrompt, setFormPrompt] = useState<Number>(0);
+    const [triggerSubmit, setTriggerSubmit] = useState<Number>(0);
+    const [inputArray, setInputArray] = useState<Array<string>>([]);
     // const textInput = React.useRef(null);
     // const { register, handleSubmit, setValue } = useForm({
     //   defaultValues: {
