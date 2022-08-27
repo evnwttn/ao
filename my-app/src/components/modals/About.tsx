59c39a70423e2a54f91adbfbfb88d73648c08fa3
evnwttn <evnwttn@gmail.com>
2022-08-27 11:00:44 -0230

    imports

---
+++ my-app/src/components/modals/About.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+import { Box } from "@mui/material";
+import { modalCenteredSx } from "../../assets/theme";
--- my-app/src/components/modals/ContactModalContent.tsx
+++ my-app/src/components/modals/ContactModalContent.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { useState, useRef, useEffect } from "react";
 import { TextField, IconButton, Box, CircularProgress } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
