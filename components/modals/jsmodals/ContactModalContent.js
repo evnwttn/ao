88058a66b436cb78fcbe7ae907a0fc16f04cbbb0
evnwttn <evnwttn@gmail.com>
2022-08-27 13:28:34 +0000

    Deploying to gh-pages from @ evnwttn/ao@8656cbab724bfa689e3f56c214d4db6258c90dcb 🚀

--- components/modals/jsmodals/ContactModalContent.js
+++ components/modals/jsmodals/ContactModalContent.js
@@ -7,32 +7,32 @@ exports.ContactModalContent = void 0;
 const react_1 = require("react");
 const material_1 = require("@mui/material");
 const Send_1 = __importDefault(require("@mui/icons-material/Send"));
-const theme_1 = require("../../assets/theme");
-const axios = require("axios").default;
-const [formData, setFormData] = (0, react_1.useState)();
-const [formLoading, setFormLoading] = (0, react_1.useState)(false);
-const nameField = (0, react_1.useRef)();
-const emailField = (0, react_1.useRef)();
-const messageField = (0, react_1.useRef)();
-(0, react_1.useEffect)(() => {
-    if (formData) {
-        setFormLoading(true);
-        axios
-            .post("http://localhost:5000/contact", Object.assign({}, formData))
-            .then((_data) => console.log(JSON.stringify(_data)))
-            .catch(function (error) {
-            console.log(error);
-        });
-    }
-}, [formData, axios]);
-(0, react_1.useEffect)(() => {
-    setTimeout(() => {
-        if (formLoading) {
-            setFormLoading(false);
-        }
-    }, 2250);
-}, [formLoading]);
+const theme_1 = require("../../../assets/theme");
 const ContactModalContent = () => {
+    const axios = require("axios").default;
+    const [formData, setFormData] = (0, react_1.useState)();
+    const [formLoading, setFormLoading] = (0, react_1.useState)(false);
+    const nameField = (0, react_1.useRef)();
+    const emailField = (0, react_1.useRef)();
+    const messageField = (0, react_1.useRef)();
+    (0, react_1.useEffect)(() => {
+        if (formData) {
+            setFormLoading(true);
+            axios
+                .post("http://localhost:5000/contact", Object.assign({}, formData))
+                .then((_data) => console.log(JSON.stringify(_data)))
+                .catch(function (error) {
+                console.log(error);
+            });
+        }
+    }, [formData, axios]);
+    (0, react_1.useEffect)(() => {
+        setTimeout(() => {
+            if (formLoading) {
+                setFormLoading(false);
+            }
+        }, 2250);
+    }, [formLoading]);
     return (React.createElement(material_1.Box, { sx: theme_1.modalCenteredSx },
         React.createElement(material_1.TextField, { placeholder: "Name", inputRef: nameField, variant: "standard", margin: "normal", sx: { width: "28vw" } }),
         React.createElement(material_1.TextField, { placeholder: "Email Address", inputRef: emailField, variant: "standard", margin: "normal", sx: { width: "28vw" } }),
