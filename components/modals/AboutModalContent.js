65d7dcc2b5169813b094932602edb1bee6afd3c0
evnwttn <evnwttn@gmail.com>
2022-08-27 13:36:11 +0000

    Deploying to gh-pages from @ evnwttn/ao@2c7850696627a9be1c5db0eb6f9aa89df3e152f4 🚀

--- components/modals/AboutModalContent.js
+++ components/modals/AboutModalContent.js
@@ -1,14 +1,18 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AboutModalContent = void 0;
+const react_1 = __importDefault(require("react"));
 const material_1 = require("@mui/material");
 const theme_1 = require("../../assets/theme");
 const AboutModalContent = () => {
-    return (React.createElement(material_1.Box, { sx: theme_1.modalCenteredSx },
+    return (react_1.default.createElement(material_1.Box, { sx: theme_1.modalCenteredSx },
         "A VISUAL ALBUM MANAGEMENT TOOLKIT DESIGNED TO TRACK & MONITOR PROGRESS, FOSTER COLLABORATION AND AID IN THE SMASHING OF GOALS.",
-        React.createElement(material_1.Box, { sx: { mt: "2vw" } },
+        react_1.default.createElement(material_1.Box, { sx: { mt: "2vw" } },
             "TO BEGIN START A ",
-            React.createElement("b", null, "NEW"),
+            react_1.default.createElement("b", null, "NEW"),
             " PROJECT.")));
 };
 exports.AboutModalContent = AboutModalContent;
