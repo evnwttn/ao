fb99ddca42a7d5e8895cde288627f56ced7b8fc4
evnwttn <evnwttn@gmail.com>
2022-08-27 11:03:29 -0230

    Update About.tsx

--- my-app/src/components/modals/About.tsx
+++ my-app/src/components/modals/About.tsx
@@ -1,3 +1,15 @@
 import React from "react";
 import { Box } from "@mui/material";
 import { modalCenteredSx } from "../../assets/theme";
+
+export const AboutModalContent = () => {
+    return (
+        <Box sx={modalCenteredSx}>
+          A VISUAL ALBUM MANAGEMENT TOOLKIT DESIGNED TO TRACK &amp; MONITOR
+          PROGRESS, FOSTER COLLABORATION AND AID IN THE SMASHING OF GOALS.
+          <Box sx={{ mt: "2vw" }}>
+            TO BEGIN START A <b>NEW</b> PROJECT.
+          </Box>
+        </Box>
+      );
+}
