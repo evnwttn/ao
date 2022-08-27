bb8119a0244d0c1c4217a4f03674e15b00819bc2
evnwttn <evnwttn@gmail.com>
2022-08-27 12:39:59 -0230

    Update NewModal.tsx

---
+++ my-app/src/components/modals/NewModal.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { NewModalContentTextfieldForm } from "./NewModalContentTextfieldForm";
+import { NewModalContentListForm } from "./NewModalContentListForm";
+
+export const NewModalContent = () => {
+    const axios = require("axios").default;
+    return <></>
+}
