6c7a629b23a1c45629ff7942fee570bbec13cf6b
evnwttn <evnwttn@gmail.com>
2022-08-27 15:10:58 +0000

    Deploying to gh-pages from @ evnwttn/ao@bb8119a0244d0c1c4217a4f03674e15b00819bc2 🚀

--- components/modals/NewModal.js
+++ components/modals/NewModal.js
@@ -1,1 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NewModalContent = void 0;
+const react_1 = __importDefault(require("react"));
+const NewModalContent = () => {
+    const axios = require("axios").default;
+    return react_1.default.createElement(react_1.default.Fragment, null);
+};
+exports.NewModalContent = NewModalContent;
