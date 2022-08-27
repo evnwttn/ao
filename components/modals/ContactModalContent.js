4466378eb6622101085c671b18f13c1f9d5cc456
evnwttn <evnwttn@gmail.com>
2022-08-27 15:14:59 +0000

    Deploying to gh-pages from @ evnwttn/ao@1163dd2753cf10ab81db853b9e5e9ff24d3f3ccd 🚀

--- components/modals/ContactModalContent.js
+++ components/modals/ContactModalContent.js
@@ -1,26 +1,48 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ContactModalContent = void 0;
-const react_1 = __importDefault(require("react"));
-const react_2 = require("react");
+const react_1 = __importStar(require("react"));
 const material_1 = require("@mui/material");
 const Send_1 = __importDefault(require("@mui/icons-material/Send"));
 const theme_1 = require("../../assets/theme");
 const ContactModalContent = () => {
     const axios = require("axios").default;
-    const [formData, setFormData] = (0, react_2.useState)({
+    const [formData, setFormData] = (0, react_1.useState)({
         name: '',
         email: '',
         message: '',
     });
-    const [formLoading, setFormLoading] = (0, react_2.useState)(false);
-    const nameField = (0, react_2.useRef)(null);
-    const emailField = (0, react_2.useRef)(null);
-    const messageField = (0, react_2.useRef)(null);
-    (0, react_2.useEffect)(() => {
+    const [formLoading, setFormLoading] = (0, react_1.useState)(false);
+    const nameField = (0, react_1.useRef)(null);
+    const emailField = (0, react_1.useRef)(null);
+    const messageField = (0, react_1.useRef)(null);
+    (0, react_1.useEffect)(() => {
         if (formData.message !== '') {
             setFormLoading(true);
             axios
@@ -31,7 +53,7 @@ const ContactModalContent = () => {
             });
         }
     }, [formData, axios]);
-    (0, react_2.useEffect)(() => {
+    (0, react_1.useEffect)(() => {
         setTimeout(() => {
             if (formLoading) {
                 setFormLoading(false);
--- components/modals/NewModal.js
+++ components/modals/NewModal.js
@@ -6,7 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.NewModalContent = void 0;
 const react_1 = __importDefault(require("react"));
 const NewModalContent = () => {
-    const axios = require("axios").default;
+    // const axios = require("axios").default;
+    // const [sessionData, setSessionData] = useState();
+    // const [startNewSession, setStartNewSession] = useState(false);
+    // const [formPrompt, setFormPrompt] = useState(0);
+    // const [triggerSubmit, setTriggerSubmit] = useState(0);
+    // const [inputArray, setInputArray] = useState([]);
+    // const textInput = React.useRef(null);
+    // const { register, handleSubmit, setValue } = useForm({
+    //   defaultValues: {
+    //     parameters: ["title"],
+    //   },
+    // });
     return react_1.default.createElement(react_1.default.Fragment, null);
 };
 exports.NewModalContent = NewModalContent;
