19f73a52e4468a7bdd7aa998ca6f6dfb145a644c
evnwttn <evnwttn@gmail.com>
2022-08-30 23:52:50 +0000

    Deploying to gh-pages from @ evnwttn/ao@0a8f32536420ef676b487e7f7964bae4251b45b0 🚀

--- components/MainLogo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MainLogo = void 0;
-const material_1 = require("@mui/material");
-const theme_1 = require("../assets/theme");
-const MainLogo = () => {
-    return (React.createElement(material_1.Box, { sx: Object.assign(Object.assign({}, theme_1.logoSx), { display: "flex", alignContent: "center", justifyContent: "center", color: theme_1.palette.aoBlue, fontSize: "13vw", mt: "-5vh" }) }, "\uA735"));
-};
-exports.MainLogo = MainLogo;
--- components/ModalButton.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ModalButton = void 0;
-const material_1 = require("@mui/material");
-const ModalButton = ({ text, handleOpen }) => {
-    return (React.createElement(material_1.Button, { disableRipple: true, key: text, onClick: () => handleOpen(text), variant: "text", color: text === "New" ? "primary" : text === "Load" ? "secondary" : "inherit" }, text));
-};
-exports.ModalButton = ModalButton;
