6ac099e60769c8ee708f72f39b491bd482091bfd
evnwttn <evnwttn@gmail.com>
2022-09-14 21:00:09 -0230

    Update AOGrid.js

--- my-app/src/components/AOGrid.js
+++ my-app/src/components/AOGrid.js
@@ -64,7 +64,7 @@ export const AOGrid = () => {
   useEffect(() => {
     updatedArray &&
       axios
-        .put("http://localhost:5000/session", {
+        .put(`http://localhost:5000/session/${updatedArray.id}`, {
           ...updatedArray,
         })
         .then((_data) => console.log(JSON.stringify(_data)))
