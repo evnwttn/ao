134faeeb3ea0c003262f3a6107324b060243c88b
evnwttn <evnwttn@gmail.com>
2022-10-10 16:50:11 -0230

    newuser imputs

--- my-app/src/components/modals/LoginNewUser.tsx
+++ my-app/src/components/modals/LoginNewUser.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Box, TextField, IconButton, InputAdornment } from "@mui/material";
+import { modalCenteredSx, modalFontSx, palette } from "../../assets/theme";
+import SendIcon from "@mui/icons-material/Send";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
-export const LoginNewUser = () => {
+export const LoginNewUser = ({
+  emailField,
+  passwordField,
+  showPassword,
+  setShowPassword,
+  isNewUser,
+  setIsNewUser,
+  sendUserLoginData,
+}: any) => {
   return <>New User</>;
 };
