bbb4008e042e466f3bf98d403ab48a4c88ef15a0
evnwttn <evnwttn@gmail.com>
2022-10-10 16:39:58 -0230

    DOM/COM SHIT

--- my-app/src/components/modals/LoginExistingUser.tsx
+++ my-app/src/components/modals/LoginExistingUser.tsx
@@ -1,5 +1,65 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { Box, TextField, IconButton, InputAdornment } from "@mui/material";
+import { modalCenteredSx, modalFontSx, palette } from "../../assets/theme";
+import SendIcon from "@mui/icons-material/Send";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
-export const LoginExistingUser = () => {
-  return <>Existing User</>;
+export const LoginExistingUser = ({
+  emailField,
+  passwordField,
+  showPassword,
+  setShowPassword,
+  isNewUser,
+  setIsNewUser,
+  sendUserLoginData,
+}: any) => {
+  return (
+    <Box sx={modalCenteredSx}>
+      <TextField
+        placeholder="Email"
+        inputRef={emailField}
+        variant="standard"
+        margin="normal"
+        sx={{ width: "26vw" }}
+      />
+      <TextField
+        placeholder="Password"
+        inputRef={passwordField}
+        variant="standard"
+        type={showPassword ? "password" : "text"}
+        margin="normal"
+        sx={{ width: "26vw" }}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton onClick={() => setShowPassword(!showPassword)}>
+                {showPassword ? (
+                  <VisibilityOff sx={{ color: palette.aoDivider }} />
+                ) : (
+                  <Visibility />
+                )}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+      <IconButton
+        disableRipple
+        sx={{ cursor: "default" }}
+        onClick={sendUserLoginData}
+      >
+        <SendIcon
+          sx={{
+            ml: "23.5vw",
+          }}
+        />
+      </IconButton>
+      <Box
+        sx={{ ...modalFontSx, mt: "1vw", cursor: "pointer" }}
+        onClick={() => setIsNewUser(!isNewUser)}
+      >
+        New User?
+      </Box>
+    </Box>
+  );
 };
--- my-app/src/components/modals/LoginModalContent.tsx
+++ my-app/src/components/modals/LoginModalContent.tsx
@@ -4,6 +4,7 @@ import { modalCenteredSx, modalFontSx, palette } from "../../assets/theme";
 import SendIcon from "@mui/icons-material/Send";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { UserLoginData } from "../../types";
+import { LoginExistingUser } from "./LoginExistingUser";
 
 export const LoginModalContent = () => {
   const axios = require("axios").default;
@@ -34,53 +35,15 @@ export const LoginModalContent = () => {
 
   return (
     !isNewUser && (
-      <Box sx={modalCenteredSx}>
-        <TextField
-          placeholder="Email"
-          inputRef={emailField}
-          variant="standard"
-          margin="normal"
-          sx={{ width: "26vw" }}
-        />
-        <TextField
-          placeholder="Password"
-          inputRef={passwordField}
-          variant="standard"
-          type={showPassword ? "password" : "text"}
-          margin="normal"
-          sx={{ width: "26vw" }}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton onClick={() => setShowPassword(!showPassword)}>
-                  {showPassword ? (
-                    <VisibilityOff sx={{ color: palette.aoDivider }} />
-                  ) : (
-                    <Visibility />
-                  )}
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
-        <IconButton
-          disableRipple
-          sx={{ cursor: "default" }}
-          onClick={sendUserLoginData}
-        >
-          <SendIcon
-            sx={{
-              ml: "23.5vw",
-            }}
-          />
-        </IconButton>
-        <Box
-          sx={{ ...modalFontSx, mt: "1vw", cursor: "pointer" }}
-          onClick={() => setIsNewUser(!isNewUser)}
-        >
-          New User?
-        </Box>
-      </Box>
+      <LoginExistingUser
+        emailField={emailField}
+        passwordField={passwordField}
+        showPassword={showPassword}
+        setShowPassword={setShowPassword}
+        isNewUser={isNewUser}
+        setIsNewUser={setIsNewUser}
+        sendUserLoginData={sendUserLoginData}
+      />
     )
   );
 };
