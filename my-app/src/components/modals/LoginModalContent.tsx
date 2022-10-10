c1d1164d8ac268a0eca6707332fbf38300b1a113
evnwttn <evnwttn@gmail.com>
2022-10-10 14:15:53 -0230

    :palm_tree:

--- my-app/src/components/modals/LoginModalContent.tsx
+++ my-app/src/components/modals/LoginModalContent.tsx
@@ -7,5 +7,9 @@ import { UserLoginData } from "../../types";
 export const LoginModalContent = () => {
   const axios = require("axios").default;
   const [userLoginData, setUserLoginData] = useState<UserLoginData>();
+  const [formLoading, setFormLoading] = useState<Boolean>(false);
+  const emailField = useRef<HTMLInputElement>(null);
+  const passwordField = useRef<HTMLInputElement>(null);
+
   return <Box sx={modalCenteredSx}>Login</Box>;
 };
