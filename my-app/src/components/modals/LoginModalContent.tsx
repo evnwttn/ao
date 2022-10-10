d3fbd926871f3a2bd8743b0123e88ee05a275f21
evnwttn <evnwttn@gmail.com>
2022-10-10 14:14:37 -0230

    applying type to UserLoginData

--- my-app/src/components/modals/LoginModalContent.tsx
+++ my-app/src/components/modals/LoginModalContent.tsx
@@ -2,15 +2,10 @@ import React, { useState, useRef } from "react";
 import { Box, TextField, IconButton, CircularProgress } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import { modalCenteredSx, circularProgressSx } from "../../assets/theme";
+import { UserLoginData } from "../../types";
 
 export const LoginModalContent = () => {
   const axios = require("axios").default;
-  const [userLoginData, setUserLoginData] = useState<{
-    email: string;
-    password: string;
-  }>({
-    email: "",
-    password: "",
-  });
+  const [userLoginData, setUserLoginData] = useState<UserLoginData>();
   return <Box sx={modalCenteredSx}>Login</Box>;
 };
