e0c3d2155b18a7af2ce3122d12f4f50c6f6c42ff
evnwttn <evnwttn@gmail.com>
2022-10-10 14:08:55 -0230

    userLoginData

--- my-app/src/components/modals/LoginModalContent.tsx
+++ my-app/src/components/modals/LoginModalContent.tsx
@@ -4,5 +4,13 @@ import SendIcon from "@mui/icons-material/Send";
 import { modalCenteredSx, circularProgressSx } from "../../assets/theme";
 
 export const LoginModalContent = () => {
+  const axios = require("axios").default;
+  const [userLoginData, setUserLoginData] = useState<{
+    email: string;
+    password: string;
+  }>({
+    email: "",
+    password: "",
+  });
   return <Box sx={modalCenteredSx}>Login</Box>;
 };
