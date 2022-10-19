d1c1fbec0c44ca2416f2174f5abe2829cd120938
evnwttn <evnwttn@gmail.com>
2022-10-19 18:39:48 -0230

    color wheel tsx

--- my-app/src/components/AOColorWheel.tsx
+++ my-app/src/components/AOColorWheel.tsx
@@ -1,22 +1,23 @@
+import React from "react";
 import { Box } from "@mui/system";
 import { AOSpoke } from "./index";
 import { palette, svgShapes } from "../assets/theme";
 
-export const AOColorWheel = ({ handleClick, openComment }) => {
+export const AOColorWheel = ({ handleClick, openComment }: any) => {
   return (
     <Box>
       <Box>
         <AOSpoke
           color={palette.aoYellow}
           shape={svgShapes.topLeft}
-          handleClick={(color) => handleClick(color)}
+          handleClick={(color: any) => handleClick(color)}
           toolTip="In Progress"
           placement="left-start"
         />
         <AOSpoke
           color={palette.aoBlue}
           shape={svgShapes.topRight}
-          handleClick={(color) => handleClick(color)}
+          handleClick={(color: any) => handleClick(color)}
           toolTip="Completed"
           placement="right-start"
         />
@@ -25,7 +26,7 @@ export const AOColorWheel = ({ handleClick, openComment }) => {
         <AOSpoke
           color={palette.aoGrey}
           shape={svgShapes.bottomLeft}
-          handleClick={(color) => handleClick(color)}
+          handleClick={(color: any) => handleClick(color)}
           toolTip="Set Task"
           placement="left-end"
         />
