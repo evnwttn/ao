9591431cf8c112c32e38e6897d658bb3199b331a
evnwttn <evnwttn@gmail.com>
2022-11-05 15:24:51 -0230

    Update NavMenuContent.tsx

--- my-app/src/components/modals/NavMenuContent.tsx
+++ my-app/src/components/modals/NavMenuContent.tsx
@@ -1,7 +1,21 @@
 import React from "react";
-import { Box } from "@mui/material";
 import { Link } from "react-router-dom";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 import { logoSx } from "../../assets/theme";
+
+const menuItems = [
+  { id: "Home" },
+  { id: "New" },
+  { id: "Load" },
+  { id: "Log Out" },
+];
 
 export const NavMenuContent = () => {
   return (
